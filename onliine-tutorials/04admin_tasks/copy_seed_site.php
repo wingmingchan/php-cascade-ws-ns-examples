300be763d27100d82579f07eb203fb5ac572a596
Wing Ming Chan <chanw@upstate.edu>
2016-01-28 10:29:29 -0500

    Reformatted the program.

--- onliine-tutorials/04admin_tasks/copy_seed_site.php
+++ onliine-tutorials/04admin_tasks/copy_seed_site.php
@@ -31,75 +31,75 @@ $role_name              = "Default";
 
 try
 {
-	try // if the site already exists
-	{
-		$new_site = $cascade->getSite( $new_site_name );
-		setSitePermissions( $cascade, $new_site_name );
-		echo "The site $new_site_name already exists!" . BR;
-	}
-	catch( e\NoSuchSiteException $e )
-	{
-		// create new site
-		$service->siteCopy( $seed_site_id, $seed_site_name, $new_site_name );
-	
-		// wait until the site is ready
-		sleep( 10 );
+    try // if the site already exists
+    {
+        $new_site = $cascade->getSite( $new_site_name );
+        setSitePermissions( $cascade, $new_site_name );
+        echo "The site $new_site_name already exists!" . BR;
+    }
+    catch( e\NoSuchSiteException $e )
+    {
+        // create new site
+        $service->siteCopy( $seed_site_id, $seed_site_name, $new_site_name );
+    
+        // wait until the site is ready
+        sleep( 10 );
 
-		if( $service->isSuccessful() )
-		{
-			echo "Site $new_site_name copied successfully." . BR;
+        if( $service->isSuccessful() )
+        {
+            echo "Site $new_site_name copied successfully." . BR;
 
-			// check if the group already exists
-			$group = $cascade->getGroup( $new_group_name );
+            // check if the group already exists
+            $group = $cascade->getGroup( $new_group_name );
 
-			if( isset( $group ) )
-			{
-				echo "Group $new_group_name already exists." . BR;
-			}
-			else
-			{
-				// create new group
-				if( $cascade->hasRoleName( $role_name ) )
-				{
-					$group = $cascade->createGroup( $new_group_name, $role_name );
-					echo "Group created." . BR;
-				}
-			}
-		
-			echo "Setting permissions" . BR;
-			setSitePermissions( $cascade, $new_site_name );
-		
-			// let it finish
-			sleep( 10 );
-		
-			createDestinationForSites( $cascade, $new_site_name );
-		
-			$site = $cascade->getAsset( a\Site::TYPE, $new_site_name );
-			
-			// the site name is also the folder name
-			$site->setUrl( 'http://www.upstate.edu/' . $new_site_name )->
-				setRecycleBinExpiration( a\Site::FIFTEEN )->
-				setLinkCheckerEnabled( true )->edit();
-			
-			sleep( 5 );
-			
-			// the config blocks
-			$site_info_block = $cascade->getAsset( a\DataBlock::TYPE, '_site-info', $new_site_name );
-			$site_info_block->setText( 'title', $new_site_title )->edit();
-		
-			$footer_contact_block = $cascade->getAsset( a\DataBlock::TYPE, '_footer-contact', $new_site_name );
-			$footer_contact_block->
-				setText( 'name', $new_site_contact_name )->
-				setText( 'email', $new_site_contact_email )->
-				edit();			
-	
-			echo BR . "Successfully set up access rights for the new site.";
-		}
-		else
-		{
-			echo "Failed to create the site. " . $service->getMessage();
-		}
-	}
+            if( isset( $group ) )
+            {
+                echo "Group $new_group_name already exists." . BR;
+            }
+            else
+            {
+                // create new group
+                if( $cascade->hasRoleName( $role_name ) )
+                {
+                    $group = $cascade->createGroup( $new_group_name, $role_name );
+                    echo "Group created." . BR;
+                }
+            }
+        
+            echo "Setting permissions" . BR;
+            setSitePermissions( $cascade, $new_site_name );
+        
+            // let it finish
+            sleep( 10 );
+        
+            createDestinationForSites( $cascade, $new_site_name );
+        
+            $site = $cascade->getAsset( a\Site::TYPE, $new_site_name );
+            
+            // the site name is also the folder name
+            $site->setUrl( 'http://www.upstate.edu/' . $new_site_name )->
+                setRecycleBinExpiration( a\Site::FIFTEEN )->
+                setLinkCheckerEnabled( true )->edit();
+            
+            sleep( 5 );
+            
+            // the config blocks
+            $site_info_block = $cascade->getAsset( a\DataBlock::TYPE, '_site-info', $new_site_name );
+            $site_info_block->setText( 'title', $new_site_title )->edit();
+        
+            $footer_contact_block = $cascade->getAsset( a\DataBlock::TYPE, '_footer-contact', $new_site_name );
+            $footer_contact_block->
+                setText( 'name', $new_site_contact_name )->
+                setText( 'email', $new_site_contact_email )->
+                edit();            
+    
+            echo BR . "Successfully set up access rights for the new site.";
+        }
+        else
+        {
+            echo "Failed to create the site. " . $service->getMessage();
+        }
+    }
 }
 catch( \Exception $e ) 
 {
