dc5ce371b39fa2b9aa72098afd172cdeabeecc75
Wing Ming Chan <chanw@upstate.edu>
2016-02-01 14:35:27 -0500

    Initial commit

---
+++ working-with-AssetOperationHandlerService/edit_data_definition_block.php
@@ -0,0 +1,68 @@
+<?php
+require_once('cascade_ws_ns/auth_chanw.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $path = "_cascade/blocks/data/audio-player";
+    $service->read( 
+        $service->createId( a\DataBlock::TYPE, $path, "cascade-admin" ) );
+    
+    if($service->isSuccessful())
+    {
+        echo "Read successfully<br />";
+        
+        $block = $service->getReadAsset()->xhtmlDataDefinitionBlock;
+        
+        u\DebugUtility::dump( $block );
+            
+        if( $block->structuredData->
+            structuredDataNodes->
+            structuredDataNode[2]->
+            structuredDataNodes->
+            structuredDataNode[0]->text == 'Example Audio Player' )
+        {
+            // edit the text
+            $block->structuredData->
+                structuredDataNodes->
+                structuredDataNode[2]->
+                structuredDataNodes->
+                structuredDataNode[0]->text = 'Audio Player';
+            
+            $asset = new \stdClass();
+            $asset->xhtmlDataDefinitionBlock = $block;
+            
+            $service->edit( $asset );
+            
+            if($service->isSuccessful())
+            {
+                echo "Edited block successfully<br />";
+            }
+            else
+            {
+                echo "Failed to edit block<br />";
+            }
+        }
+        else if( $block->structuredData->
+            structuredDataNodes->
+            structuredDataNode[2]->
+            structuredDataNodes->
+            structuredDataNode[0]->text == 'Audio Player' )
+        {
+            echo "The text has already been changed<br />";
+        }
+    }
+    else
+        echo "Failed to read. " . $service->getMessage();
+}
+catch( \Exception $e )
+{
+    echo $e;
+}
+?>
