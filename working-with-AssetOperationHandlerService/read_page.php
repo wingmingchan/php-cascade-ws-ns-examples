4d49efa4ae04461c9f61288fe356821373c9c0bb
Wing Ming Chan <chanw@upstate.edu>
2016-02-05 09:07:06 -0500

    Initial commit

---
+++ working-with-AssetOperationHandlerService/read_page.php
@@ -0,0 +1,29 @@
+<?php
+require_once('cascade_ws_ns/auth_chanw.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $page_path = "index";
+    
+    $page = $service->retrieve( $service->createId( a\Page::TYPE, $page_path, "cascade-admin" ) );
+
+    if( $service->isSuccessful() )
+    {
+        echo "Read successfully";
+        u\DebugUtility::dump( $page );
+    }
+    else
+        echo "Failed to read. " . $service->getMessage();
+}
+catch( \Exception $e )
+{
+    echo $e;
+}
+?>
