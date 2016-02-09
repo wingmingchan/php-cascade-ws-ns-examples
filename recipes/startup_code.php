fb68c1c6c8a6563682487d7715b7dba3496f4334
Wing Ming Chan <chanw@upstate.edu>
2016-02-09 15:46:35 -0500

    Initial commit

---
+++ recipes/startup_code.php
@@ -0,0 +1,19 @@
+<?php 
+require_once('cascade_ws_ns/auth_chanw.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+?>
