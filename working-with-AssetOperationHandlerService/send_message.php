86c9449efe19face9da4538bb1ab218f3264b187
Wing Ming Chan <chanw@upstate.edu>
2016-02-09 14:32:35 -0500

    Initial commit

---
+++ working-with-AssetOperationHandlerService/send_message.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Note that the message sent will be displayed on the dashboard as one of the messages.
+*/
+require_once('cascade_ws_ns/auth_chanw.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+	$message          = new \stdClass();
+	$message->to      = 'test'; // a group
+	$message->from    = 'chanw';
+	$message->subject = 'test';
+	$message->body    = 'This is a test. This is only a test.';
+	
+	$service->sendMessage( $message );
+
+	if($service->isSuccessful())
+	{
+		echo "Message sent successfully<br />";
+		$reply = $service->getReply();
+		var_dump( $reply );
+	}
+	else
+	{
+		echo "Failed to send the message. " . $service->getMessage();
+	}
+}
+catch( \Exception $e )
+{
+    echo $e;
+}
+?>
