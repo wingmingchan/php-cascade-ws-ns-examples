25513e82242c9752c16d342fa950e8bd2d96cebd
Wing Ming Chan <chanw@upstate.edu>
2016-02-09 12:40:34 -0500

    Initial commit

---
+++ working-with-AssetOperationHandlerService/read_all_users.php
@@ -0,0 +1,32 @@
+<?php
+require_once('cascade_ws_ns/auth_chanw.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $group_name = "ALL-USERS";
+    
+    $group = $service->retrieve( $service->createId( a\Group::TYPE, $group_name ) );
+
+    if( $service->isSuccessful() )
+    {
+        echo "Read successfully";
+        u\DebugUtility::dump( $group );
+        
+        $users = explode( ";", $group->users );
+        u\DebugUtility::dump( $users );
+    }
+    else
+        echo "Failed to read. " . $service->getMessage();
+}
+catch( \Exception $e )
+{
+    echo $e;
+}
+?>
