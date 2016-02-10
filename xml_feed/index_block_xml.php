9ead424ed120ace175f2ecedf60baa539319e952
Wing Ming Chan <chanw@upstate.edu>
2016-02-10 08:43:53 -0500

    Reformatted the code.

--- xml_feed/index_block_xml.php
+++ xml_feed/index_block_xml.php
@@ -18,37 +18,37 @@ use cascade_ws_exception as e;
 
 try
 {
-	// retrieve the block ID
-	$block_id = $_GET[ "block_id" ];
-	
-	if( isset( $block_id ) &&  $service->isHexString( $block_id ) )
-	{
-		// site containing the page
-		$site_name = "cascade-admin";
-	
-		// the page used to expose index block XML
-		$page_path = "/intra/xml/index";
-	
-		// the block containing XML to be exposed
-		$block = $cascade->getAsset( a\IndexBlock::TYPE, $block_id );
-	
-		// retrieve the page
-		$page = $cascade->getAsset( a\Page::TYPE, $page_path, $site_name );
-	
-		// attach the block to the page's default region and publish it
-		$page->setRegionBlock( 'XML', 'DEFAULT', $block )->edit()->publish();
-	
-		sleep( 10 );
-	
-		// retrieve and output the XML
-		$url = "http://www.upstate.edu/" . $site_name . $page_path . ".xml";
-		$file_content = file_get_contents( $url );
-		echo $file_content;
-	}
-	else
-	{
-		echo "<error>No valid block ID supplied.</error>";
-	}
+    // retrieve the block ID
+    $block_id = $_GET[ "block_id" ];
+    
+    if( isset( $block_id ) &&  $service->isHexString( $block_id ) )
+    {
+        // site containing the page
+        $site_name = "cascade-admin";
+    
+        // the page used to expose index block XML
+        $page_path = "/intra/xml/index";
+    
+        // the block containing XML to be exposed
+        $block = $cascade->getAsset( a\IndexBlock::TYPE, $block_id );
+    
+        // retrieve the page
+        $page = $cascade->getAsset( a\Page::TYPE, $page_path, $site_name );
+    
+        // attach the block to the page's default region and publish it
+        $page->setRegionBlock( 'XML', 'DEFAULT', $block )->edit()->publish();
+    
+        sleep( 10 );
+    
+        // retrieve and output the XML
+        $url = "http://www.upstate.edu/" . $site_name . $page_path . ".xml";
+        $file_content = file_get_contents( $url );
+        echo $file_content;
+    }
+    else
+    {
+        echo "<error>No valid block ID supplied.</error>";
+    }
 }
 catch( \Exception $e ) 
 {
