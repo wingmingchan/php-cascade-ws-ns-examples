ecfdf51da4074563530f99c9f90332783b1c0222
Wing Ming Chan <chanw@upstate.edu>
2016-02-12 14:37:24 -0500

    Updated the comments.

--- xml_feed/index_block_xml.php
+++ xml_feed/index_block_xml.php
@@ -1,8 +1,8 @@
 <?php
 /*
 This program can be used to create a feed,
-containing the block XML of an index block. The block id must be
-passed in as part of the query string. The url of this program
+containing the block XML of an index block. The block id or path/site
+must be passed in as part of the query string. The url of this program
 then can be fed into a feed block and the block will be populated
 with the retrieved XML. 
 */
@@ -18,7 +18,7 @@ use cascade_ws_exception as e;
 
 try
 {
-    // retrieve the block ID
+    // retrieve the block ID, path, and site name
     $block_id   = ( isset( $_GET[ "block_id" ] ) ? $_GET[ "block_id" ] : "" );
     $path       = ( isset( $_GET[ "path" ] )     ? $_GET[ "path" ] : "" );
     $site       = ( isset( $_GET[ "site" ] )     ? $_GET[ "site" ] : "" );
@@ -41,7 +41,7 @@ try
         $page_path = "/intra/xml/index";
     
         // the block containing XML to be exposed
-        $block = $cascade->getAsset( a\IndexBlock::TYPE, $block_id );
+        $block = $cascade->getAsset( a\IndexBlock::TYPE, $id_path, $site );
     
         // retrieve the page
         $page = $cascade->getAsset( a\Page::TYPE, $page_path, $site_name );
