10ec21888af71eb95208cf46fcd370a33a2ade32
Wing Ming Chan <chanw@upstate.edu>
2016-02-16 08:28:18 -0500

    Reformatted the code.

--- asset-class-test-code/container.php
+++ asset-class-test-code/container.php
@@ -9,12 +9,12 @@ use cascade_ws_exception as e;
 
 try
 {
-	$folder = $cascade->getAsset( a\Folder::TYPE, "e637b2d98b7f08ee71e0db967edcd450" );
-	$folder->deleteAllChildren();
-	
-	$folder = $cascade->getAsset( a\Folder::TYPE, "e636ef478b7f08ee71e0db96b386c7f8" );
-	u\DebugUtility::dump( $folder->getChildren() );
-	u\DebugUtility::dump( $folder->getContainerChildrenIds() );
+    $folder = $cascade->getAsset( a\Folder::TYPE, "e637b2d98b7f08ee71e0db967edcd450" );
+    $folder->deleteAllChildren();
+    
+    $folder = $cascade->getAsset( a\Folder::TYPE, "e636ef478b7f08ee71e0db96b386c7f8" );
+    u\DebugUtility::dump( $folder->getChildren() );
+    u\DebugUtility::dump( $folder->getContainerChildrenIds() );
 }
 catch( \Exception $e ) 
 {
