7d695590ba30babbaddf13be8317bac5dc90fd60
Wing Ming Chan <chanw@upstate.edu>
2016-03-01 15:15:21 -0500

    Initial commit

---
+++ recipes/create_user.php
@@ -0,0 +1,47 @@
+<?php 
+require_once('cascade_ws_ns/auth_wing.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+	$site_name         = 'web-service-tutorial';
+    $global_role_name  = 'Manager';
+    $site_role_name    = 'Site Test Role';
+    $group_name        = 'web-service-tutorial-group';
+    $username          = 'web-service-tutorial-user';
+    $password          = '1234';
+    
+    if( is_null( $cascade->getUser( $username ) ) )
+    {
+		$site        = $cascade->getAsset( a\Site::TYPE, $site_name );
+		$group       = $cascade->getAsset( a\Group::TYPE, $group_name );
+		$global_role = $cascade->getRoleAssetByName( $global_role_name );
+		
+		$cascade->
+			createUser(
+				$username,
+				$password,
+				$group,
+				$global_role
+			)->
+			enable()->
+			setDefaultGroup( $group )->
+			setDefaultSite( $site );                
+		echo "The user $username has been created.";
+	}
+	else
+	{
+		echo "The user $username already exists.";
+	}
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+?>
