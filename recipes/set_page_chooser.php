a34823342e978bdde20012a44e4c9e18dad6d630
Wing Ming Chan <chanw@upstate.edu>
2016-03-02 09:38:03 -0500

    Initial commit

---
+++ recipes/set_page_chooser.php
@@ -0,0 +1,23 @@
+<?php 
+require_once('cascade_ws_ns/auth_wing.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+	$block = $cascade->getAsset( a\DataBlock::TYPE, "e10da00f8b7f0856005cdf95fc73eeca" );
+	
+	$block->setPage( 
+		"group;page-chooser",
+		$cascade->getAsset( a\Page::TYPE, "87e6d0cf8b7f0856002a5e11c8e6bd21" ) )->edit();
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+?>
