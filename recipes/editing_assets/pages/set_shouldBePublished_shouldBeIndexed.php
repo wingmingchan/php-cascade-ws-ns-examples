a1886e873df5954f6b0b466b47c93d6de5ef9faf
Wing Ming Chan <chanw@upstate.edu>
2016-04-18 13:22:46 -0400

    Initial commit

---
+++ recipes/editing_assets/pages/set_shouldBePublished_shouldBeIndexed.php
@@ -0,0 +1,21 @@
+<?php 
+require_once('cascade_ws_ns/auth_chanw.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $page = $cascade->getAsset( a\Page::TYPE, '2a47653d8b7f08ee3c48c4e996f9054a' );
+    $page->setShouldBePublished( false )->
+        setShouldBeIndexed( false )->edit();
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+?>
