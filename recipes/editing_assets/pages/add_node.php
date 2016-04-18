89bb6486dd63f061ea406fd9d474fee66f7af1c4
Wing Ming Chan <chanw@upstate.edu>
2016-04-18 14:49:35 -0400

    Initial commit

---
+++ recipes/editing_assets/pages/add_node.php
@@ -0,0 +1,22 @@
+<?php 
+require_once('cascade_ws_ns/auth_chanw.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $page = $cascade->getAsset( a\Page::TYPE, '2a47653d8b7f08ee3c48c4e996f9054a' );
+    
+    // create a copy of the node and append it to the end
+	$page->appendSibling( "content-group;0" );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+?>
