c1a5a6e9af37ff32872fc3d6814104b78db6d8b1
Wing Ming Chan <chanw@upstate.edu>
2016-04-18 14:15:56 -0400

    Initial commit

---
+++ recipes/editing_assets/pages/swap_data_nodes.php
@@ -0,0 +1,22 @@
+<?php 
+require_once('cascade_ws_ns/auth_chanw.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $page = $cascade->getAsset( a\Page::TYPE, '2a47653d8b7f08ee3c48c4e996f9054a' );
+
+    // swap the two instances of a multiple group
+    $page->swapData( "content-group;0", "content-group;1" );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+?>
