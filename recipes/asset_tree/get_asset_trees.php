a5af8076d87360411e89664de00dd5d4ada6edd0
Wing Ming Chan <chanw@upstate.edu>
2016-04-22 08:05:12 -0400

    Initial commit

---
+++ recipes/asset_tree/get_asset_trees.php
@@ -0,0 +1,24 @@
+<?php 
+require_once('cascade_ws_ns/auth_chanw.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $site = $cascade->getSite( "cascade-admin" );
+    
+    $base_folder_asset_tree   = $site->getBaseFolderAssetTree();
+    $asset_factory_asset_tree = $site->getRootAssetFactoryContainerAssetTree();
+    $metadata_set_asset_tree  = $site->getRootMetadataSetContainerAssetTree();
+    $content_type_asset_tree  = $site->getRootContentTypeContainerAssetTree();
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+?>
