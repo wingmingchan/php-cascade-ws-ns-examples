c19de410f9415c7691692d332356d1c2f4cc352f
Wing Ming Chan <chanw@upstate.edu>
2016-04-25 10:04:47 -0400

    Initial commit

---
+++ recipes/asset_tree/search_asset_by_name.php
@@ -0,0 +1,29 @@
+<?php 
+require_once('cascade_ws_ns/auth_chanw.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $results = array();
+
+    $cascade->getAsset( a\Site::TYPE, 'cascade-admin' )->
+        getBaseFolderAssetTree()->traverse(
+            array( a\DataBlock::TYPE => array( c\F::SEARCH_BY_NAME ) ),
+            array( c\F::SEARCH_BY_NAME => 
+                array( a\DataBlock::TYPE => array( 'name'=> 'monster_with_phantom' ) ) ),
+            $results
+        );
+
+    u\DebugUtility::dump( $results );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+?>
