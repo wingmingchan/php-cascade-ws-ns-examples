45231872b25219b5c50137c4ace6bbb7886bf065
Wing Ming Chan <chanw@upstate.edu>
2016-05-05 11:44:56 -0400

    Initial commit

---
+++ recipes/data_definitions/get_structured_data_object.php
@@ -0,0 +1,25 @@
+<?php
+require_once('cascade_ws_ns/auth_chanw.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try 
+{
+    $dd = $cascade->getAsset( 
+        a\DataDefinition::TYPE, "5f4526bb8b7f08ee76b12c417a2b982b" );
+        
+    // an StructuredData object
+    u\DebugUtility::dump( $dd->getStructuredDataObject() );
+    //u\DebugUtility::dump( $dd->getStructuredDataObject()->toStdClass() );
+    //u\DebugUtility::dump( $dd->getStructuredDataObject()->getIdentifiers() );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE;
+}
+?>
