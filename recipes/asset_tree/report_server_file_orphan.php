f8d2a8554745047707ef3ac2795af198de8c0d02
Wing Ming Chan <chanw@upstate.edu>
2016-05-13 09:33:05 -0400

    Initial commit

---
+++ recipes/asset_tree/report_server_file_orphan.php
@@ -0,0 +1,90 @@
+<?php
+require_once('cascade_ws_ns/auth_chanw.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try 
+{
+	// folder to check
+	$folder_name = 'files';
+	$site_name   = 'cascade-admin';
+	// server path to folder
+	$host        = "/server/www/$site_name/";
+
+	// store results
+	$results = array();
+	
+	$cascade
+		->getAsset( a\Folder::TYPE, $folder_name, $site_name )
+		->getAssetTree()
+		->traverse( 
+			array( 
+				a\File::TYPE =>   array( c\F::REPORT_ORPHANS )
+			), 
+			NULL, 
+			$results );
+
+	echo S_H3, "Cascade Orphans:", E_H3;
+	u\DebugUtility::dump( $results );
+	echo BR;
+	
+	$server_files = array();
+	processFolder( $folder_name, $server_files );
+	
+	echo S_H3, "Server Orphans:", E_H3;
+	u\DebugUtility::dump( $server_files );
+	echo BR;
+
+	$difference = array();
+	
+	foreach( $server_files as $file )
+	{
+		// compare the two and store the difference
+		if( !in_array( $file, $results[ c\F::REPORT_ORPHANS ][ "file" ] ) )
+		{
+			$difference[] = $file;
+		}
+	}
+
+	sort( $difference );
+
+	echo S_H3, "Difference:", E_H3;
+	u\DebugUtility::dump( $difference );
+	echo BR;
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE;
+}
+
+function processFolder( $dir, &$array ) 
+{
+	global $host;
+	
+    if( is_dir( $host . $dir . "/" ) && $handle = opendir( $host . $dir . "/" ) ) 
+    {
+        while( false !== ( $file = readdir( $handle ) ) ) 
+        {
+            if( $file == '.' || $file == '..' ) 
+            {
+                continue;
+            }
+            else if( is_file( $host . $dir . "/" . $file ) ) 
+            {
+                $name    = $dir . "/" . $file;
+                $array[] = $name;
+                //echo $name, BR;
+            }
+            else if( is_dir( $host . $dir . "/" . $file . "/" ) ) 
+            {
+                processFolder( $dir . "/" . $file, $array );
+            }
+        }
+    }
+}
+?>
---
+++ recipes/asset_tree/report_server_page_orphan.php
@@ -0,0 +1,95 @@
+<?php
+require_once('cascade_ws_ns/auth_chanw.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try 
+{
+	$site_name   = 'cascade-admin';
+	// server path to folder
+	$host        = "/server/www/";
+
+	// store results
+	$results = array();
+	
+	// collect all pages in Cascade
+	$cascade
+		->getAsset( a\Folder::TYPE, "/", $site_name )
+		->getAssetTree()
+		->traverse( 
+			array( 
+				a\Page::TYPE => array( "assetTreeStoreAssetPath" )
+			), 
+			NULL, 
+			$results );
+
+	echo S_H3, "Cascade Pages:", E_H3;
+	u\DebugUtility::dump( $results );
+	echo BR;
+	
+	$server_pages = array();
+	processFolder( $site_name, $server_pages );
+	
+	echo S_H3, "Server Pages:", E_H3;
+	u\DebugUtility::dump( $server_pages );
+	echo BR;
+
+	$difference = array();
+	
+	foreach( $server_pages as $page )
+	{
+		$page_path = substr( $page, strlen( $site_name ) + 1 ); // the slash
+		$page_path = substr( $page_path, 0, -4 );  // remove site name and .php
+		
+		// compare the two and store the difference
+		if( !in_array( $page_path, $results[ "assetTreeStoreAssetPath" ] ) )
+		{
+			$difference[] = $page_path;
+		}
+	}
+
+	sort( $difference );
+
+	echo S_H3, "Difference:", E_H3;
+	u\DebugUtility::dump( $difference );
+	echo BR;
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE;
+}
+
+function processFolder( $dir, &$array ) 
+{
+	global $host;
+	
+    if( is_dir( $host . $dir . "/" ) && $handle = opendir( $host . $dir . "/" ) ) 
+    {
+        while( false !== ( $file = readdir( $handle ) ) ) 
+        {
+            if( $file == '.' || $file == '..' ) 
+            {
+                continue;
+            }
+            else if( is_file( $host . $dir . "/" . $file ) ) 
+            {
+                $name    = $dir . "/" . $file;
+                
+                if( u\StringUtility::endsWith( $name, ".php" ) )
+                {
+                	$array[] = $name;
+                }
+            }
+            else if( is_dir( $host . $dir . "/" . $file . "/" ) ) 
+            {
+                processFolder( $dir . "/" . $file, $array );
+            }
+        }
+    }
+}
+?>
