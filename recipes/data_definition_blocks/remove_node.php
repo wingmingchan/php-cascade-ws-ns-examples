9f1e8d5724a045dd6c6cf5ade6cb9f393dd59eba
Wing Ming Chan <chanw@upstate.edu>
2016-05-31 14:08:27 -0400

    Initial commit

---
+++ recipes/data_definition_blocks/remove_node.php
@@ -0,0 +1,19 @@
+<?php
+require_once('cascade_ws_ns/auth_chanw.php');
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+try 
+{
+    $dd_block = $cascade->getAsset( 
+        a\DataDefinitionBlock::TYPE, "a4a1a0068b7f0856011c5ec6fc26bfa8" );
+    $dd_block->removeLastSibling( "group;group-multiple-first;0" ); // FQI of first node
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE;
+}
+?>
