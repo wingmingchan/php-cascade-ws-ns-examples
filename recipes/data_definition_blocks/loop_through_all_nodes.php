55190b6046043c0ad614c4f6bfcbed427150326d
Wing Ming Chan <chanw@upstate.edu>
2016-05-31 14:36:18 -0400

    Initial commit

---
+++ recipes/data_definition_blocks/loop_through_all_nodes.php
@@ -0,0 +1,28 @@
+<?php
+require_once('cascade_ws_ns/auth_chanw.php');
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try 
+{
+    $dd_block = $cascade->getAsset( 
+        a\DataDefinitionBlock::TYPE, "a4a1a0068b7f0856011c5ec6fc26bfa8" );
+    $fqi = $dd_block->getIdentifiers();
+    
+    foreach( $fqi as $id )
+    {
+        if( $dd_block->isTextNode( $id ) )
+        {
+            echo $dd_block->getText( $id ), BR;
+        }
+    }
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE;
+}
+?>
