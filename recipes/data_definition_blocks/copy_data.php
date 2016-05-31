cbcbd57d64ff2511cc676ef7d869a1c3087a9df8
Wing Ming Chan <chanw@upstate.edu>
2016-05-31 15:10:51 -0400

    Initial commit

---
+++ recipes/data_definition_blocks/copy_data.php
@@ -0,0 +1,23 @@
+<?php
+require_once('cascade_ws_ns/auth_chanw.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+try 
+{
+    $source_block = $cascade->getAsset( 
+        a\DataDefinitionBlock::TYPE, "a4a1a0068b7f0856011c5ec6fc26bfa8" );
+    $target_block = $cascade->getAsset( 
+        a\DataDefinitionBlock::TYPE, "07ff55e98b7f08ee601529410c10bcaa" );
+        
+    $source_block->copyDataTo( $target_block );  
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE;
+}
+?>
