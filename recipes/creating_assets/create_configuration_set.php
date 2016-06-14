87ea871bafc886cd736edca30a970d252c91b3f8
Wing Ming Chan <chanw@upstate.edu>
2016-06-14 09:33:08 -0400

    Reformatted the code.

--- recipes/creating_assets/create_configuration_set.php
+++ recipes/creating_assets/create_configuration_set.php
@@ -11,62 +11,62 @@ use cascade_ws_exception as e;
 try
 {
     $site_name        = 'web-service-tutorial';
-	$desktop_template = 
-		$cascade->getAsset(
-			a\Template::TYPE, 'templates/three-columns', $site_name );
-	$xml_template     = 
-		$cascade->getAsset( a\Template::TYPE, 'templates/xml', $site_name );
+    $desktop_template = 
+        $cascade->getAsset(
+            a\Template::TYPE, 'templates/three-columns', $site_name );
+    $xml_template     = 
+        $cascade->getAsset( a\Template::TYPE, 'templates/xml', $site_name );
 
-	// create a text block to be attached to a region at the config level
-	$block_folder  =
-		$cascade->getAsset( a\Folder::TYPE, 'blocks', $site_name );
-	$code = "<div id='top-graphics'>Top Graphics</div>";
+    // create a text block to be attached to a region at the config level
+    $block_folder  =
+        $cascade->getAsset( a\Folder::TYPE, 'blocks', $site_name );
+    $code = "<div id='top-graphics'>Top Graphics</div>";
 
-	$block_name = 'top-graphics';
-	$block      = $cascade->getTextBlock( 'blocks/' . $block_name, $site_name );
-	
-	if( is_null( $block ) )
-	{
-		$block = $cascade->createTextBlock(
-			$block_folder,
-			$block_name,
-			$code );
-	}
-	
-	$csc_name = 'Test Configuration Set Container';
-	$csc      = $cascade->getPageConfigurationSetContainer( $csc_name, $site_name );
+    $block_name = 'top-graphics';
+    $block      = $cascade->getTextBlock( 'blocks/' . $block_name, $site_name );
+    
+    if( is_null( $block ) )
+    {
+        $block = $cascade->createTextBlock(
+            $block_folder,
+            $block_name,
+            $code );
+    }
+    
+    $csc_name = 'Test Configuration Set Container';
+    $csc      = $cascade->getPageConfigurationSetContainer( $csc_name, $site_name );
 
-	if( is_null( $csc ) )
-	{
-		// create configuration set container
-		$csc = $cascade->createPageConfigurationSetContainer(
-			$cascade->getAsset( 
-				a\PageConfigurationSetContainer::TYPE, '/', $site_name ),
-			'Test Configuration Set Container'
-		);
-	}
-	
-	$pcs_name = 'Three Columns';
-	$pcs      = $cascade->getPageConfigurationSet( $csc_name . '/' . $pcs_name, $site_name );
+    if( is_null( $csc ) )
+    {
+        // create configuration set container
+        $csc = $cascade->createPageConfigurationSetContainer(
+            $cascade->getAsset( 
+                a\PageConfigurationSetContainer::TYPE, '/', $site_name ),
+            'Test Configuration Set Container'
+        );
+    }
+    
+    $pcs_name = 'Three Columns';
+    $pcs      = $cascade->getPageConfigurationSet( $csc_name . '/' . $pcs_name, $site_name );
 
-	if( is_null( $pcs ) )
-	{
-		// create configuration set with default configuration
-		$pcs = $cascade->createPageConfigurationSet(
-			$csc,              // parent container
-			$pcs_name,         // configuration set name
-			'Desktop',         // default configuration name
-			$desktop_template, // template
-			'.php',            // file extension
-			c\T::HTML          // serialization type
-		)->
-		// attach a block to a region at config level
-		setConfigurationPageRegionBlock( 
-			'Desktop', 'TOP GRAPHICS', $block )->
-		edit();
-		// add xml configuration
-		$pcs->addPageConfiguration( 'XML', $xml_template, '.xml', c\T::XML );        
-	}
+    if( is_null( $pcs ) )
+    {
+        // create configuration set with default configuration
+        $pcs = $cascade->createPageConfigurationSet(
+            $csc,              // parent container
+            $pcs_name,         // configuration set name
+            'Desktop',         // default configuration name
+            $desktop_template, // template
+            '.php',            // file extension
+            c\T::HTML          // serialization type
+        )->
+        // attach a block to a region at config level
+        setConfigurationPageRegionBlock( 
+            'Desktop', 'TOP GRAPHICS', $block )->
+        edit();
+        // add xml configuration
+        $pcs->addPageConfiguration( 'XML', $xml_template, '.xml', c\T::XML );        
+    }
 }
 catch( \Exception $e ) 
 {
