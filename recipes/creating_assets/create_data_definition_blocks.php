a38ae1d3575709d1cdc9e98f1dfd691f2a94292b
Wing Ming Chan <chanw@upstate.edu>
2016-06-27 15:08:49 -0400

    Initial commit

---
+++ recipes/creating_assets/create_data_definition_blocks.php
@@ -0,0 +1,55 @@
+<?php 
+require_once('cascade_ws_ns/auth_sandbox.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $site_name  = 'web-service-tutorial';
+    $block_name = 'xhtml-block';
+    $block      = $cascade->getDataBlock( 'blocks/' . $block_name, $site_name );
+    
+    if( is_null( $block ) )
+    {
+        // create an xhtml block to be attached to a region at the page level
+        $block_folder =
+            $cascade->getAsset( a\Folder::TYPE, 'blocks', $site_name );
+        $code = "<p>Some content.</p>";
+
+        $cascade->createXhtmlBlock(
+            $block_folder,
+            $block_name,
+            $code );
+    }
+    
+    $block_name = 'simple-text-block';
+    $block      = $cascade->getDataBlock( 'blocks/' . $block_name, $site_name );
+    
+    if( is_null( $block ) )
+    {
+        // create a data definition block 
+        // to be attached to a region at the page level
+        $data_definition =
+            $cascade->getAsset( 
+                a\DataDefinition::TYPE, 
+                'Test Data Definition Container/Simple Text', $site_name );
+        $block_folder =
+            $cascade->getAsset( a\Folder::TYPE, 'blocks', $site_name );
+        $cascade->createDataDefinitionBlock(
+            $block_folder,
+            $block_name,
+            $data_definition )->
+        setText( 'text', 'Some content for the data block.' )->
+        edit();
+    }        
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+?>
