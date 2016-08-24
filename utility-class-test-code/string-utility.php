cab28abc65a41d8be69cb9d1623771b9a82d662a
Wing Ming Chan <chanw@upstate.edu>
2016-08-24 10:11:04 -0400

    Reformatted the code.

--- utility-class-test-code/string-utility.php
+++ utility-class-test-code/string-utility.php
@@ -10,26 +10,26 @@ use cascade_ws_exception as e;
 
 try
 {
-	echo ( u\StringUtility::startsWith( "Hello", "He" ) ? "yes" : "no" ), BR;
-	
-	echo ( u\StringUtility::startsWith( "Hello", "e" ) ? "yes" : "no" ), BR;
+    echo ( u\StringUtility::startsWith( "Hello", "He" ) ? "yes" : "no" ), BR;
+    
+    echo ( u\StringUtility::startsWith( "Hello", "e" ) ? "yes" : "no" ), BR;
 
-	echo u\StringUtility::removeSiteNameFromPath( "site://cascade-admin/web-services/api/utility-classes/debug-utility" ), BR;
+    echo u\StringUtility::removeSiteNameFromPath( "site://cascade-admin/web-services/api/utility-classes/debug-utility" ), BR;
 
-	echo u\StringUtility::getParentPathFromPath( "/web-services/api/utility-classes/debug-utility" ), BR;
+    echo u\StringUtility::getParentPathFromPath( "/web-services/api/utility-classes/debug-utility" ), BR;
 
-	echo u\StringUtility::getNameFromPath( "/web-services/api/utility-classes/debug-utility" ), BR;
+    echo u\StringUtility::getNameFromPath( "/web-services/api/utility-classes/debug-utility" ), BR;
 
-	echo u\StringUtility::getMethodName( "structuredData" ), BR;
+    echo u\StringUtility::getMethodName( "structuredData" ), BR;
 
-	u\DebugUtility::dump( u\StringUtility::getExplodedStringArray( ";", "this;0;that;3;these" ) );
+    u\DebugUtility::dump( u\StringUtility::getExplodedStringArray( ";", "this;0;that;3;these" ) );
 
-	echo u\StringUtility::getFullyQualifiedIdentifierWithoutPositions( "this;0;that;3;these" ), BR;
+    echo u\StringUtility::getFullyQualifiedIdentifierWithoutPositions( "this;0;that;3;these" ), BR;
 
-	echo ( u\StringUtility::endsWith( "Hello", "lo" ) ? "yes" : "no" ), BR;
-	
-	echo ( u\StringUtility::endsWith( "Hello", "l" ) ? "yes" : "no" ), BR;
-	
+    echo ( u\StringUtility::endsWith( "Hello", "lo" ) ? "yes" : "no" ), BR;
+    
+    echo ( u\StringUtility::endsWith( "Hello", "l" ) ? "yes" : "no" ), BR;
+    
     echo u\ReflectionUtility::getClassDocumentation( "cascade_ws_utility\StringUtility", true );
 }
 catch( \Exception $e ) 
