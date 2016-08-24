684c7967071153d55ddf46a3d36ecf0eb935e72d
Wing Ming Chan <chanw@upstate.edu>
2016-08-24 09:17:42 -0400

    Reformatted the code

--- utility-class-test-code/reflection-utility.php
+++ utility-class-test-code/reflection-utility.php
@@ -10,67 +10,67 @@ use cascade_ws_exception as e;
 
 try
 {
-	u\ReflectionUtility::showMethodSignatures( 
-		"cascade_ws_utility\ReflectionUtility", true );
-		
-	u\ReflectionUtility::showMethodSignature( 
-		"cascade_ws_utility\ReflectionUtility", "showMethodInfo", true );
+    u\ReflectionUtility::showMethodSignatures( 
+        "cascade_ws_utility\ReflectionUtility", true );
+        
+    u\ReflectionUtility::showMethodSignature( 
+        "cascade_ws_utility\ReflectionUtility", "showMethodInfo", true );
 
-	u\ReflectionUtility::showMethodReturnType(
-	    "cascade_ws_utility\ReflectionUtility", "getMethodInfo", true );
+    u\ReflectionUtility::showMethodReturnType(
+        "cascade_ws_utility\ReflectionUtility", "getMethodInfo", true );
 
-	u\ReflectionUtility::showMethodInfo(
-	    "cascade_ws_utility\ReflectionUtility", "getMethodInfo", true );
+    u\ReflectionUtility::showMethodInfo(
+        "cascade_ws_utility\ReflectionUtility", "getMethodInfo", true );
 
-	u\ReflectionUtility::showMethodException(
-	    "cascade_ws_utility\ReflectionUtility", "getMethodInfo", true );
+    u\ReflectionUtility::showMethodException(
+        "cascade_ws_utility\ReflectionUtility", "getMethodInfo", true );
 
-	u\ReflectionUtility::showMethodExample(
-	    "cascade_ws_utility\ReflectionUtility", "getMethodInfo", true );
+    u\ReflectionUtility::showMethodExample(
+        "cascade_ws_utility\ReflectionUtility", "getMethodInfo", true );
    
-	u\ReflectionUtility::showMethodDescription(
-	    "cascade_ws_utility\ReflectionUtility", "getMethodSignature", true );
- 	
-	u\ReflectionUtility::showFunctionSignature( "str_replace", true );
+    u\ReflectionUtility::showMethodDescription(
+        "cascade_ws_utility\ReflectionUtility", "getMethodSignature", true );
+     
+    u\ReflectionUtility::showFunctionSignature( "str_replace", true );
 
-	u\ReflectionUtility::showClassPostscript( "cascade_ws_utility\ReflectionUtility", true );
-	
-	u\ReflectionUtility::showClassDescription( "cascade_ws_utility\ReflectionUtility", true );
-	
-	echo u\ReflectionUtility::getNameSpace( "cascade_ws_asset\Page" );
+    u\ReflectionUtility::showClassPostscript( "cascade_ws_utility\ReflectionUtility", true );
+    
+    u\ReflectionUtility::showClassDescription( "cascade_ws_utility\ReflectionUtility", true );
+    
+    echo u\ReflectionUtility::getNameSpace( "cascade_ws_asset\Page" );
 
-	echo u\ReflectionUtility::getMethodSignatures( 
-	    "cascade_ws_utility\ReflectionUtility", "getMethodSignature" ), BR;
-	    
-	echo u\ReflectionUtility::getMethodSignatureByName( 
-	    "cascade_ws_utility\ReflectionUtility", "getMethodSignature" ), BR;
-	    
-	echo u\ReflectionUtility::getMethodSignature( 
-	    new \ReflectionMethod( "cascade_ws_utility\ReflectionUtility", "getMethodSignature" ) ), BR;
+    echo u\ReflectionUtility::getMethodSignatures( 
+        "cascade_ws_utility\ReflectionUtility", "getMethodSignature" ), BR;
+        
+    echo u\ReflectionUtility::getMethodSignatureByName( 
+        "cascade_ws_utility\ReflectionUtility", "getMethodSignature" ), BR;
+        
+    echo u\ReflectionUtility::getMethodSignature( 
+        new \ReflectionMethod( "cascade_ws_utility\ReflectionUtility", "getMethodSignature" ) ), BR;
 
-	u\DebugUtility::dump( u\ReflectionUtility::getMethods( $service ) );
+    u\DebugUtility::dump( u\ReflectionUtility::getMethods( $service ) );
 
-	echo u\ReflectionUtility::getMethodInfoByName(
-	    "cascade_ws_utility\ReflectionUtility", "getMethodInfo" );
-	    
-	echo u\ReflectionUtility::getMethodInfo( 
-	    new \ReflectionMethod( "cascade_ws_utility\ReflectionUtility", "getMethodInfo" ) );
-	
-	u\DebugUtility::dump( u\ReflectionUtility::getMethod( $service, "read" ) );
+    echo u\ReflectionUtility::getMethodInfoByName(
+        "cascade_ws_utility\ReflectionUtility", "getMethodInfo" );
+        
+    echo u\ReflectionUtility::getMethodInfo( 
+        new \ReflectionMethod( "cascade_ws_utility\ReflectionUtility", "getMethodInfo" ) );
+    
+    u\DebugUtility::dump( u\ReflectionUtility::getMethod( $service, "read" ) );
 
-	echo u\ReflectionUtility::getFunctionSignature( new \ReflectionFunction( "str_getcsv" ) );
-	
-	echo u\ReflectionUtility::getClassPostscript( "cascade_ws_utility\ReflectionUtility", NULL, true );
-	
-	echo u\ReflectionUtility::getClassName( $service );
-	
-	echo u\ReflectionUtility::getClassDescription( "cascade_ws_utility\ReflectionUtility", NULL, true );
+    echo u\ReflectionUtility::getFunctionSignature( new \ReflectionFunction( "str_getcsv" ) );
+    
+    echo u\ReflectionUtility::getClassPostscript( "cascade_ws_utility\ReflectionUtility", NULL, true );
+    
+    echo u\ReflectionUtility::getClassName( $service );
+    
+    echo u\ReflectionUtility::getClassDescription( "cascade_ws_utility\ReflectionUtility", NULL, true );
 
-	echo u\ReflectionUtility::getClassPostScript( "cascade_ws_utility\ReflectionUtility", NULL, true );
+    echo u\ReflectionUtility::getClassPostScript( "cascade_ws_utility\ReflectionUtility", NULL, true );
 
-	echo u\ReflectionUtility::getClassDocumentation( "cascade_ws_utility\ReflectionUtility", true );
+    echo u\ReflectionUtility::getClassDocumentation( "cascade_ws_utility\ReflectionUtility", true );
 
-	//echo u\ReflectionUtility::getClassDocumentation( "cascade_ws_asset\Page", true );
+    //echo u\ReflectionUtility::getClassDocumentation( "cascade_ws_asset\Page", true );
 }
 catch( \Exception $e ) 
 {
@@ -78,6 +78,6 @@ catch( \Exception $e )
 }
 catch( \Error $er )
 {
-	echo S_PRE . $er . E_PRE; 
+    echo S_PRE . $er . E_PRE; 
 }
 ?>
