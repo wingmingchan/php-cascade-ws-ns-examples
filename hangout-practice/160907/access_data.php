0105a0292b0f0edc59c60aa0af17a02cfc2dd903
wingmingchan <chanw@upstate.edu>
2016-09-03 20:38:54 -0400

    Initial commit

---
+++ hangout-practice/160907/access_data.php
@@ -0,0 +1,32 @@
+<?php
+require_once('auth_tutorial.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+	$page     = $cascade->getAsset( a\Page::TYPE, "1b399c028b7ffe83164c9314f7323a98" );
+	$data     = $page->getProperty()->structuredData;
+	$metadata = $page->getProperty()->metadata;
+	
+	u\DebugUtility::dump( $data );
+	u\DebugUtility::dump( $metadata );
+	
+	echo $data->structuredDataNodes->structuredDataNode[ 0 ]->identifier, BR;
+	echo $metadata->dynamicFields->dynamicField[ 1 ]->name, BR;
+	
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+	echo S_PRE . $er . E_PRE; 
+}
+?>
