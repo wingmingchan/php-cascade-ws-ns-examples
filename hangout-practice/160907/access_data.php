f4ea6a1764a96c1aeaad46e40a270e3f94c93499
wingmingchan <chanw@upstate.edu>
2016-09-03 21:10:22 -0400

    Added more code

--- hangout-practice/160907/access_data.php
+++ hangout-practice/160907/access_data.php
@@ -1,5 +1,5 @@
 <?php
-require_once('auth_tutorial.php');
+require_once('auth_tutorial7.php');
 
 use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
@@ -11,15 +11,19 @@ use cascade_ws_exception as e;
 try
 {
     $page     = $cascade->getAsset( a\Page::TYPE, "1b399c028b7ffe83164c9314f7323a98" );
+
     $data     = $page->getProperty()->structuredData;
     $metadata = $page->getProperty()->metadata;
+    $region   = $page->getProperty()->pageConfigurations->pageConfiguration[ 0 ]->
+                pageRegions->pageRegion;
     
     u\DebugUtility::dump( $data );
     u\DebugUtility::dump( $metadata );
+    u\DebugUtility::dump( $region );
     
     echo $data->structuredDataNodes->structuredDataNode[ 0 ]->identifier, BR;
     echo $metadata->dynamicFields->dynamicField[ 1 ]->name, BR;
-    
+    echo $region->name, BR;
 }
 catch( \Exception $e ) 
 {
