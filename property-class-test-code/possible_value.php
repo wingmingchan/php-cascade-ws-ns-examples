334249250f6171b1be8d9eedaa329eb794f132b5
Wing Ming Chan <chanw@upstate.edu>
2016-09-08 11:36:00 -0400

    Initial commit

---
+++ property-class-test-code/possible_value.php
@@ -0,0 +1,64 @@
+<?php
+require_once('auth_tutorial7.php');
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+	$ms   = $cascade->getAsset( a\MetadataSet::TYPE, "06d5db638b7ffe83765c5582062dd782" );
+	$dmfd = $ms->getDynamicMetadataFieldDefinition( "languages" );
+	
+    u\DebugUtility::dump( $dmfd->getPossibleValues() );
+    
+    // multiselect
+    $english = $dmfd->getPossibleValue( "English" );
+	echo u\StringUtility::boolToString( $english->getSelectedByDefault() ), BR;
+	
+	foreach( $dmfd->getPossibleValues() as $pv )
+		echo $pv->getValue(), BR;
+		
+	$dmfd->getPossibleValue( "Japanese" )->setSelectedByDefault( true );
+	u\DebugUtility::dump( $dmfd->toStdClass() );
+	
+	echo u\StringUtility::boolToString( $dmfd->hasPossibleValue( "Spanish" ) ), BR;
+	
+	// radio
+	$dmfd = $ms->getDynamicMetadataFieldDefinition( "gender" );
+	$dmfd->getPossibleValue( "Female" )->setSelectedByDefault( false );
+	$dmfd->getPossibleValue( "Male" )->setSelectedByDefault( true );
+	u\DebugUtility::dump( $dmfd->toStdClass() );
+	u\DebugUtility::dump( $dmfd->getPossibleValue( "Female" )->toStdClass() );
+	
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+	echo S_PRE . $er . E_PRE; 
+}
+/*
+Useful code templates:
+    u\ReflectionUtility::showMethodSignatures( 
+        "cascade_ws_utility\ReflectionUtility" );
+        
+    u\ReflectionUtility::showMethodSignature( 
+        "cascade_ws_asset\Page", "edit" );
+        
+    u\ReflectionUtility::showMethodDescription( 
+        "cascade_ws_utility\ReflectionUtility", "getMethodInfoByName", true );
+        
+    u\ReflectionUtility::showMethodExample( 
+        "cascade_ws_utility\ReflectionUtility", "getMethodInfoByName", true );
+
+    u\DebugUtility::dump( $page );
+
+    $cascade->getAsset( a\Page::TYPE, "389b32a68b7ffe83164c931497b7bc24" )->dump( true );
+*/
+?>
