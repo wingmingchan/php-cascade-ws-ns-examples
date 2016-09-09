64d610b4d9be44b9c99db098406c7eec08599589
Wing Ming Chan <chanw@upstate.edu>
2016-09-09 12:20:08 -0400

    Added code

--- asset-class-test-code/metadata_set.php
+++ asset-class-test-code/metadata_set.php
@@ -2,9 +2,9 @@
 require_once('auth_tutorial7.php');
 
 use cascade_ws_constants as c;
-use cascade_ws_asset as a;
-use cascade_ws_property as p;
-use cascade_ws_utility as u;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
 
 $mode = 'all';
@@ -226,5 +226,9 @@ try
 catch( \Exception $e )
 {
     echo S_PRE . $e . E_PRE;
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE;
 }
 ?>
