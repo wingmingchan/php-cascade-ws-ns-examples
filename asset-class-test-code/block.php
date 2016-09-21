35f6c6c677053149fc20b44a8218e68f892099af
Wing Ming Chan <chanw@upstate.edu>
2016-09-21 12:59:56 -0400

    Added more method tests

--- asset-class-test-code/block.php
+++ asset-class-test-code/block.php
@@ -1,5 +1,5 @@
 <?php 
-require_once('cascade_ws_ns/auth_chanw.php');
+require_once('auth_tutorial7.php');
 use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
 use cascade_ws_asset     as a;
@@ -9,20 +9,33 @@ use cascade_ws_exception as e;
 try
 {
     // test static method
-    $id = "980c2b7e8b7f0856015997e4b5924ca1";
-    $block = a\Block::getBlock( $service, $id )->dump( true ); // no type info supplied
-    echo a\Block::getBlockType( $service, $id ), BR;
+    $id = "388f033b8b7ffe83164c9314c23a3f8f";
+    $block = a\Block::getBlock( $service, $id ); //->dump( true ); // no type info supplied
     
+    echo a\Block::getBlockType( $service, $id ), BR;
     // get methods
     echo "Created by: ", $block->getCreatedBy(), BR,
-        "Created on: ", $block->getCreatedDate(), BR,
-        "Expiration folder ID:", $block->getExpirationFolderId();
+         "Created on: ", $block->getCreatedDate(), BR,
+         "Expiration folder ID: ", $block->getExpirationFolderId(), BR,
+         "Expiration folder path: ", $block->getExpirationFolderPath(), BR,
+         "Expiration folder recycled: ", u\StringUtility::boolToString(
+             $block->getExpirationFolderRecycled() ), BR,
+         "Last modified by: ", $block->getLastModifiedBy(), BR,
+         "Last modified date: ", $block->getLastModifiedDate(), BR,
+         "Metadata set ID: ", $block->getMetadataSetId(), BR,
+         "Metadata set path: ", $block->getMetadataSetPath(), BR;
         
     u\DebugUtility::dump( $block->getDynamicFields() );
     u\DebugUtility::dump( $block->getMetadataStdClass() );
+    
+    echo u\ReflectionUtility::getClassDocumentation( "cascade_ws_asset\Block" );
 }
 catch( \Exception $e ) 
 {
     echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE;
 }
 ?>
