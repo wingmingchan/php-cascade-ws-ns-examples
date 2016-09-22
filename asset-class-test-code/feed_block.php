22a5079c5beb7c294668413eee70d12cba620913
Wing Ming Chan <chanw@upstate.edu>
2016-09-22 11:34:18 -0400

    Updated with more tests

--- asset-class-test-code/feed_block.php
+++ asset-class-test-code/feed_block.php
@@ -1,22 +1,21 @@
 <?php
-require_once('cascade_ws_ns/auth_chanw.php');
+require_once('auth_chanw.php');
 
 use cascade_ws_constants as c;
-use cascade_ws_asset as a;
-use cascade_ws_property as p;
-use cascade_ws_utility as u;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
 
 $mode = 'all';
 //$mode = 'display';
 //$mode = 'dump';
-//$mode = 'get';
 //$mode = 'set';
 //$mode = 'raw';
 
 try
 {
-    $id = "a174ab188b7f08560139425ceeed850b"; // test-xml
+    $id = "d051de5f8b7f085601675d706ac3325c"; // test-xml
     $fb  = $cascade->getAsset( a\FeedBlock::TYPE, $id );
     
     switch( $mode )
@@ -62,7 +61,7 @@ try
             $url = "http://www.upstate.edu/scripts/cascade/" . 
             "process_item.php?cat=bluepages&id=bp1077";
             
-            $fb->setFeedURL( $url )->edit()->dump( true );
+            $fb->setFeedURL( $url )->edit()->dump();
         
             if( $mode != 'all' )
                 break;
@@ -77,9 +76,15 @@ try
             if( $mode != 'all' )
                 break;
     }
+    
+    u\ReflectionUtility::showMethodSignatures( "cascade_ws_asset\FeedBlock" );
 }
 catch( \Exception $e )
 {
     echo S_PRE . $e . E_PRE;
 }
+catch( \Error $er ) 
+{
+    echo S_PRE . $er . E_PRE; 
+} 
 ?>
