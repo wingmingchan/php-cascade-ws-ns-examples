7c15ba68b0fb9efedd84d208419ec464fc55617f
Wing Ming Chan <chanw@upstate.edu>
2016-10-18 13:56:33 -0400

    Initial commit

--- property-class-test-code/structured_data_node.php
+++ property-class-test-code/structured_data_node.php
@@ -73,82 +73,82 @@ array(16) {
 */
 try
 {
-	// multiple
-	$block = $cascade->getAsset( a\DataBlock::TYPE, "1f21fcc48b7ffe834c5fe91e2704e2d0" );
-	// everything
-	//$block = $cascade->getAsset( a\DataBlock::TYPE, "1f21fb798b7ffe834c5fe91e7f6d784f" );
-	//u\DebugUtility::dump( $block->getIdentifiers() );
-	$sd    = $block->getStructuredData();
-	$map   = $sd->getIdentifierNodeMap();
-	
-	$node = $map[ "group" ];
-	//$node->setText( "10-17-2016" );
-	//u\DebugUtility::dump( $node->toStdClass() );
-	
-	
-	//$node->display()->dump();
+    // multiple
+    $block = $cascade->getAsset( a\DataBlock::TYPE, "1f21fcc48b7ffe834c5fe91e2704e2d0" );
+    // everything
+    //$block = $cascade->getAsset( a\DataBlock::TYPE, "1f21fb798b7ffe834c5fe91e7f6d784f" );
+    //u\DebugUtility::dump( $block->getIdentifiers() );
+    $sd    = $block->getStructuredData();
+    $map   = $sd->getIdentifierNodeMap();
+    
+    $node = $map[ "group" ];
+    //$node->setText( "10-17-2016" );
+    //u\DebugUtility::dump( $node->toStdClass() );
+    
+    
+    //$node->display()->dump();
 
-/*	
-	echo u\StringUtility::getCoalescedString( $node->getAssetType() ), BR;
-	echo u\StringUtility::getCoalescedString( $node->getBlockId() ), BR;
-	echo u\StringUtility::getCoalescedString( $node->getBlockPath() ), BR;
-	echo u\StringUtility::getCoalescedString( $node->getFileId() ), BR;
-	echo u\StringUtility::getCoalescedString( $node->getFilePath() ), BR;
-	echo u\StringUtility::getCoalescedString( $node->getLinkableId() ), BR;
-	echo u\StringUtility::getCoalescedString( $node->getLinkablePath() ), BR;
-	echo u\StringUtility::getCoalescedString( $node->getPageId() ), BR;
-	echo u\StringUtility::getCoalescedString( $node->getPagePath() ), BR;
-	echo u\StringUtility::getCoalescedString( $node->getSymlinkId() ), BR;
-	echo u\StringUtility::getCoalescedString( $node->getSymlinkPath() ), BR;
-*/	
-	//echo u\StringUtility::getCoalescedString( $node->getParentId() ), BR;
-/*	
-	echo $node->getIdentifier(), BR;
-	
-	u\DebugUtility::dump( $node->getItems() );
-	u\DebugUtility::dump( $node->getPossibleValues() );
-	echo u\StringUtility::boolToString( $node->getRecycled() ), BR;
-	
-	echo $node->getText(), BR;
-	echo u\StringUtility::getCoalescedString( $node->getTextNodeType() ), BR;
-	echo u\StringUtility::getCoalescedString( $node->getType() ), BR;
+/*    
+    echo u\StringUtility::getCoalescedString( $node->getAssetType() ), BR;
+    echo u\StringUtility::getCoalescedString( $node->getBlockId() ), BR;
+    echo u\StringUtility::getCoalescedString( $node->getBlockPath() ), BR;
+    echo u\StringUtility::getCoalescedString( $node->getFileId() ), BR;
+    echo u\StringUtility::getCoalescedString( $node->getFilePath() ), BR;
+    echo u\StringUtility::getCoalescedString( $node->getLinkableId() ), BR;
+    echo u\StringUtility::getCoalescedString( $node->getLinkablePath() ), BR;
+    echo u\StringUtility::getCoalescedString( $node->getPageId() ), BR;
+    echo u\StringUtility::getCoalescedString( $node->getPagePath() ), BR;
+    echo u\StringUtility::getCoalescedString( $node->getSymlinkId() ), BR;
+    echo u\StringUtility::getCoalescedString( $node->getSymlinkPath() ), BR;
+*/    
+    //echo u\StringUtility::getCoalescedString( $node->getParentId() ), BR;
+/*    
+    echo $node->getIdentifier(), BR;
+    
+    u\DebugUtility::dump( $node->getItems() );
+    u\DebugUtility::dump( $node->getPossibleValues() );
+    echo u\StringUtility::boolToString( $node->getRecycled() ), BR;
+    
+    echo $node->getText(), BR;
+    echo u\StringUtility::getCoalescedString( $node->getTextNodeType() ), BR;
+    echo u\StringUtility::getCoalescedString( $node->getType() ), BR;
 */
-/*	
-	echo u\StringUtility::boolToString( $node->hasItem( "Maybe" ) ), BR;
-	echo u\StringUtility::boolToString( $node->IsAsset() ), BR;
-	echo u\StringUtility::boolToString( $node->isBlockChooser() ), BR;
-	echo u\StringUtility::boolToString( $node->isCalendar() ), BR;
-	echo u\StringUtility::boolToString( $node->isCheckbox() ), BR;
-	echo u\StringUtility::boolToString( $node->isDatetime() ), BR;
-	echo u\StringUtility::boolToString( $node->isDropdown() ), BR;
-	echo u\StringUtility::boolToString( $node->isFileChooser() ), BR;
-	echo u\StringUtility::boolToString( $node->isGroup() ), BR;
-	echo u\StringUtility::boolToString( $node->isLinkableChooser() ), BR;
-	echo u\StringUtility::boolToString( $node->isMultiLine() ), BR;
-	//echo u\StringUtility::boolToString( $node->isMultiple() ), BR;
-	//echo u\StringUtility::boolToString( $node->isMultiSelector() ), BR;
-	//echo u\StringUtility::boolToString( $node->isPageChooser() ), BR;
-	//echo u\StringUtility::boolToString( $node->isPageChooser() ), BR;
-	//echo u\StringUtility::boolToString( $node->isRadio() ), BR;
-	echo u\StringUtility::boolToString( $node->isRequired() ), BR;
-	echo u\StringUtility::boolToString( $node->isSymlinkChooser() ), BR;
-	echo u\StringUtility::boolToString( $node->isText() ), BR;
-	echo u\StringUtility::boolToString( $node->isTextarea() ), BR;
-	echo u\StringUtility::boolToString( $node->isTextBox() ), BR;
-	echo u\StringUtility::boolToString( $node->isWYSIWYG() ), BR;
-*/	
-	//u\DebugUtility::dump( $node->getStructuredDataNodes() );
-	
-	//u\DebugUtility::dump( $node->getIdentifierNodeMap() );
-	//u\DebugUtility::dump( $node->getChildren() );
-	//u\DebugUtility::dump( $node->getDataDefinition() );
-	$node->addChildNode( "group;group-multiple-second" );
-	u\DebugUtility::dump( $node->toStdClass() );
-	//$node->removeLastChildNode( "group;group-multiple-second" );
-	//u\DebugUtility::dump( $node->toStdClass() );
-	//echo u\StringUtillity::boolToString( $node->
-	//u\DebugUtility::dump( $node->cloneNode()->toStdClass() );
-	
+/*    
+    echo u\StringUtility::boolToString( $node->hasItem( "Maybe" ) ), BR;
+    echo u\StringUtility::boolToString( $node->IsAsset() ), BR;
+    echo u\StringUtility::boolToString( $node->isBlockChooser() ), BR;
+    echo u\StringUtility::boolToString( $node->isCalendar() ), BR;
+    echo u\StringUtility::boolToString( $node->isCheckbox() ), BR;
+    echo u\StringUtility::boolToString( $node->isDatetime() ), BR;
+    echo u\StringUtility::boolToString( $node->isDropdown() ), BR;
+    echo u\StringUtility::boolToString( $node->isFileChooser() ), BR;
+    echo u\StringUtility::boolToString( $node->isGroup() ), BR;
+    echo u\StringUtility::boolToString( $node->isLinkableChooser() ), BR;
+    echo u\StringUtility::boolToString( $node->isMultiLine() ), BR;
+    //echo u\StringUtility::boolToString( $node->isMultiple() ), BR;
+    //echo u\StringUtility::boolToString( $node->isMultiSelector() ), BR;
+    //echo u\StringUtility::boolToString( $node->isPageChooser() ), BR;
+    //echo u\StringUtility::boolToString( $node->isPageChooser() ), BR;
+    //echo u\StringUtility::boolToString( $node->isRadio() ), BR;
+    echo u\StringUtility::boolToString( $node->isRequired() ), BR;
+    echo u\StringUtility::boolToString( $node->isSymlinkChooser() ), BR;
+    echo u\StringUtility::boolToString( $node->isText() ), BR;
+    echo u\StringUtility::boolToString( $node->isTextarea() ), BR;
+    echo u\StringUtility::boolToString( $node->isTextBox() ), BR;
+    echo u\StringUtility::boolToString( $node->isWYSIWYG() ), BR;
+*/    
+    //u\DebugUtility::dump( $node->getStructuredDataNodes() );
+    
+    //u\DebugUtility::dump( $node->getIdentifierNodeMap() );
+    //u\DebugUtility::dump( $node->getChildren() );
+    //u\DebugUtility::dump( $node->getDataDefinition() );
+    $node->addChildNode( "group;group-multiple-second" );
+    u\DebugUtility::dump( $node->toStdClass() );
+    //$node->removeLastChildNode( "group;group-multiple-second" );
+    //u\DebugUtility::dump( $node->toStdClass() );
+    //echo u\StringUtillity::boolToString( $node->
+    //u\DebugUtility::dump( $node->cloneNode()->toStdClass() );
+    
 }
 catch( \Exception $e ) 
 {
