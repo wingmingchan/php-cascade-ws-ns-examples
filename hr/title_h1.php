33397a5a4d1d68bf31cd0022cab496d7f0ee86ef
Wing Ming Chan <chanw@upstate.edu>
2016-11-04 08:33:54 -0400

    Updated

--- hr/title_h1.php
+++ hr/title_h1.php
@@ -1,15 +1,16 @@
 <?php
 /*
-This program is used to fill in the mandatory
-page title and h1.
+This program is used to fill in the mandatory page title and h1.
 Note that the method call $page->getText fails
 if the page is absolutely blank. When a page contains
 no contents at all, the structured data object is also
-blank, with no data definition id. My library will treat
-such a page as an XHTML page, not a page associated with
-a data definition.
+blank. My library will treat such a page as an XHTML page,
+not a page associated with a data definition. Manually fill in
+some data so that the structured data becomes visible.
 */
-require_once('auth_tutorial7.php');
+$start_time = time();
+
+require_once('auth_chanw.php');
 
 use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
@@ -18,26 +19,27 @@ use cascade_ws_property  as p;
 use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
 
+u\DebugUtility::setTimeSpaceLimits();
+
 try
 {
     $site_name = "hrintra";
     $at = $cascade->getSite( $site_name )->getAssetTree();
-/*
-    $at = $cascade->getAsset( 
-        a\Folder::TYPE, "" )->getAssetTree();
-*/
 
     $at->traverse(
         array( a\Page::TYPE => array( "assetTreeSetPageDisplayName" ) )
     );
+    u\DebugUtility::outputDuration( $start_time );
 }
 catch( \Exception $e ) 
 {
     echo S_PRE . $e . E_PRE; 
+    u\DebugUtility::outputDuration( $start_time );
 }
 catch( \Error $er )
 {
-    echo S_PRE . $er . E_PRE; 
+	echo S_PRE . $er . E_PRE; 
+    u\DebugUtility::outputDuration( $start_time );
 }
 
 function assetTreeSetPageDisplayName(
