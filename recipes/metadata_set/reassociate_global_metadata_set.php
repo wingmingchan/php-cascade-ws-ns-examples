5aaccec1f830f3ab7212565ca526357def3d5005
wingmingchan <chanw@upstate.edu>
2016-11-29 18:26:46 -0500

    Initial commit

--- recipes/metadata_set/reassociate_global_metadata_set.php
+++ recipes/metadata_set/reassociate_global_metadata_set.php
@@ -10,48 +10,48 @@ use cascade_ws_exception as e;
 
 try
 {
-	$default_ms = $cascade->getAsset( 
-		a\MetadataSet::TYPE, "ROOT_metadataset" );
-	$asset_ids = $default_ms->getSubscribers();
-	
-	$asset_ms_map = array(
-		a\DataBlock::TYPE => $cascade->getAsset( 
-			a\MetadataSet::TYPE, "5f4526018b7f08ee76b12c413ab40518" ),
-		a\TextBlock::TYPE => $cascade->getAsset( 
-			a\MetadataSet::TYPE, "5f4526018b7f08ee76b12c413ab40518" ),
-		a\FeedBlock::TYPE => $cascade->getAsset( 
-			a\MetadataSet::TYPE, "5f4526018b7f08ee76b12c413ab40518" ),
-		a\IndexBlock::TYPE => $cascade->getAsset( 
-			a\MetadataSet::TYPE, "5f4526018b7f08ee76b12c413ab40518" ),
-		a\XmlBlock::TYPE => $cascade->getAsset( 
-			a\MetadataSet::TYPE, "5f4526018b7f08ee76b12c413ab40518" ),
-		a\File::TYPE => $cascade->getAsset( 
-			a\MetadataSet::TYPE, "5f4525fa8b7f08ee76b12c41f1334e21" ),
-		a\Folder::TYPE => $cascade->getAsset( 
-			a\MetadataSet::TYPE, "5f4526098b7f08ee76b12c412063f8b8" ),
-		a\Symlink::TYPE => $cascade->getAsset( 
-			a\MetadataSet::TYPE, "5f45261b8b7f08ee76b12c416580b064" )
-	);
-	
-	$count = 0;
-	
-	foreach( $asset_ids as $asset_id )
-	{
-		// base folder of Global
-		if( $asset_id->getId() == 'ROOT' )
-			continue;
-			
-		$asset = $asset_id->getAsset( $service );
-		$asset->setMetadataSet(
-			$asset_ms_map[ $asset->getType() ] );
-		$count++;
-		
-		// do 10 at a time
-		if( $count == 10 )
-		{
-			break;
-		}
-	}
+    $default_ms = $cascade->getAsset( 
+        a\MetadataSet::TYPE, "ROOT_metadataset" );
+    $asset_ids = $default_ms->getSubscribers();
+    
+    $asset_ms_map = array(
+        a\DataBlock::TYPE => $cascade->getAsset( 
+            a\MetadataSet::TYPE, "5f4526018b7f08ee76b12c413ab40518" ),
+        a\TextBlock::TYPE => $cascade->getAsset( 
+            a\MetadataSet::TYPE, "5f4526018b7f08ee76b12c413ab40518" ),
+        a\FeedBlock::TYPE => $cascade->getAsset( 
+            a\MetadataSet::TYPE, "5f4526018b7f08ee76b12c413ab40518" ),
+        a\IndexBlock::TYPE => $cascade->getAsset( 
+            a\MetadataSet::TYPE, "5f4526018b7f08ee76b12c413ab40518" ),
+        a\XmlBlock::TYPE => $cascade->getAsset( 
+            a\MetadataSet::TYPE, "5f4526018b7f08ee76b12c413ab40518" ),
+        a\File::TYPE => $cascade->getAsset( 
+            a\MetadataSet::TYPE, "5f4525fa8b7f08ee76b12c41f1334e21" ),
+        a\Folder::TYPE => $cascade->getAsset( 
+            a\MetadataSet::TYPE, "5f4526098b7f08ee76b12c412063f8b8" ),
+        a\Symlink::TYPE => $cascade->getAsset( 
+            a\MetadataSet::TYPE, "5f45261b8b7f08ee76b12c416580b064" )
+    );
+    
+    $count = 0;
+    
+    foreach( $asset_ids as $asset_id )
+    {
+        // base folder of Global
+        if( $asset_id->getId() == 'ROOT' )
+            continue;
+            
+        $asset = $asset_id->getAsset( $service );
+        $asset->setMetadataSet(
+            $asset_ms_map[ $asset->getType() ] );
+        $count++;
+        
+        // do 10 at a time
+        if( $count == 10 )
+        {
+            break;
+        }
+    }
 }
 catch( \Exception $e ) 
 {
@@ -59,6 +59,6 @@ catch( \Exception $e )
 }
 catch( \Error $er )
 {
-	echo S_PRE . $er . E_PRE; 
+    echo S_PRE . $er . E_PRE; 
 }
 ?>
