aac617e34a1289c89d579124ea44287d4c3c2b5d
Wing Ming Chan <chanw@upstate.edu>
2016-12-16 13:37:23 -0500

    Updated

--- asset-class-test-code/message.php
+++ asset-class-test-code/message.php
@@ -13,7 +13,31 @@ try
     $messages = $cascade->getUnpublishMessages();
     
     if( count( $messages ) > 0 )
-        $messages[ 0 ]->display();
+    {
+    	$message = $messages[ 0 ];
+        //$messages[ 0 ]->display();
+        echo $message->getBody(), BR;
+        echo $message->getBrokenLinks(), BR;
+        echo date_format( $message->getDate(), 'Y-m-d H:i:s' ), BR;
+        u\DebugUtility::dump( $message->getErrors() );
+        echo $message->getFrom(), BR;
+        echo $message->getTo(), BR;
+        echo $message->getId(), BR;
+        echo $message->getType(), BR;
+        
+        echo u\StringUtility::boolToString( $message->getIsComplete() ), BR;
+        echo $message->getJobsWithErrors(), BR;
+        echo $message->getNumberOfBrokenLinks(), BR;
+        echo $message->getNumberOfJobsWithErrors(), BR;
+        echo $message->getNumberOfSkippedJobs(), BR;
+        echo $message->getNumberOfSuccessfulJobs(), BR;
+        echo $message->getSkippedJobs(), BR;
+        echo $message->getSubject(), BR;
+        echo $message->getSuccessfulJobs(), BR;
+
+        echo u\StringUtility::boolToString( $message->getWithFailures() ), BR;
+        echo u\StringUtility::boolToString( $message->getWithIssues() ), BR;
+    }
 
 /*
     $message = $cascade->getMessage( "6ca134da8b7f08ee67a11426bae0cc0a" );
