9aa669cc1e2899d776e69369fba4d53771882a96
Wing Ming Chan <chanw@upstate.edu>
2016-12-19 10:30:54 -0500

    Updated

--- asset-class-test-code/site_destination_container.php
+++ asset-class-test-code/site_destination_container.php
@@ -1,10 +1,10 @@
 <?php
-require_once('cascade_ws_ns/auth_chanw.php');
+require_once('auth_tutorial7.php');
 
 use cascade_ws_constants as c;
-use cascade_ws_asset as a;
-use cascade_ws_property as p;
-use cascade_ws_utility as u;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
 
 $mode = 'all';
@@ -15,7 +15,7 @@ $mode = 'all';
 
 try
 {
-    $id  = "64c4d4508b7f085600ae22825cb6097c"; // Test
+    $id  = "17b343d38b7ffe834304cee2c1ceece9"; // Test
     $sdc = $cascade->getAsset( a\SiteDestinationContainer::TYPE, $id );
 
     switch( $mode )
@@ -68,9 +68,15 @@ try
             if( $mode != 'all' )
                 break;
     }
+    
+    echo u\ReflectionUtility::getClassDocumentation( "cascade_ws_asset\SiteDestinationContainer" );
 }
 catch( \Exception $e )
 {
     echo S_PRE . $e . E_PRE;
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE;
 }
 ?>
