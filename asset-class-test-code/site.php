f8bb4494cb1adbcd824439e69fe6ddf3a8ee98e1
Wing Ming Chan <chanw@upstate.edu>
2016-12-19 10:25:35 -0500

    Updated

--- asset-class-test-code/site.php
+++ asset-class-test-code/site.php
@@ -1,29 +1,25 @@
 <?php
-require_once('cascade_ws_ns/auth_chanw.php');
+require_once('auth_tutorial7.php');
 
 use cascade_ws_constants as c;
-use cascade_ws_asset as a;
-use cascade_ws_property as p;
-use cascade_ws_utility as u;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
-
-//require_once('cascade_ws/auth_sandbox.php');
 
 $mode = 'all';
 //$mode = 'display';
 //$mode = 'dump';
-//$mode = 'get';
-//$mode = 'schedule';
-//$mode = 'assignment';
-//$mode = 'set';
+$mode = 'get';
+$mode = 'assignment';
+$mode = 'set';
 //$mode = 'publish';
 //$mode = 'asset-tree';
 //$mode = 'raw';
 
 try
 {
-    $id = "tuw-test"; // tuw-test
-    //$id = "cascade-admin"; // on sandbox
+    $id = "cascade-admin-webapp"; // on sandbox
     $s  = $cascade->getAsset( a\Site::TYPE, $id );
     
     switch( $mode )
@@ -42,87 +38,45 @@ try
                 break;
                 
         case 'get':
-            // test ScheduledPublishing
-            echo "Cron expression: " . $s->getCronExpression() . BR .
-                "Days of week: " . $s->getDaysOfWeek() . BR .
-                "Publish days of week: " . BR;
-            var_dump( $s->getPublishDaysOfWeek() );
-            echo BR .
-                "Publish interval hours: " . 
-                $s->getPublishIntervalHours() . BR .
-                "Send report on error only: " . 
-                $s->getSendReportOnErrorOnly() . BR .
-                "Send report to groups: " . 
-                $s->getSendReportToGroups() . BR .
-                "Send report to users: " . 
-                $s->getSendReportToUsers() . BR .
-                "Time to publish: " . 
-                $s->getTimeToPublish() . BR;
+            echo $s->getBaseFolderId(), BR;
+            echo u\StringUtility::getCoalescedString( $s->getCssClasses() ), BR;
+            echo u\StringUtility::getCoalescedString( $s->getCssFileId() ), BR;
+            echo u\StringUtility::getCoalescedString( $s->getCssFilePath() ), BR;
+            echo u\StringUtility::boolToString( $s->getCssFileRecycled() ), BR;
+            echo $s->getDefaultMetadataSetId(), BR;
+            echo $s->getDefaultMetadataSetPath(), BR;
+            echo u\StringUtility::boolToString( $s->getExternalLinkCheckOnPublish() ), BR;
+            echo u\StringUtility::boolToString( $s->getLinkCheckerEnabled() ), BR;
+            echo $s->getRecycleBinExpiration(), BR;
                 
-            // test Site
-            echo 
-                "Site name: " . $s->getSiteName() . BR .
-                "Default metadata set ID: " . 
-                $s->getDefaultMetadataSetId() . BR .
-                
-                "";
-            if( $mode != 'all' )
-                break;
-                
-        case 'schedule':
-            // case 1: remove schedule
-            //$s->unsetScheduledPublishing()->edit();
-            //$s->setScheduledPublishing( false )->edit();
-            // case 2: exception thrown, additional value is required
-            //$s->setScheduledPublishing( true )->edit();
-            // case 3a: every Friday at 12 am
-            //$s->setScheduledPublishing( true, a\PublishSet::FRIDAY )->edit();
-            // case 3b: every Monday at 6:30 pm
-            //$s->setDayOfWeek( a\PublishSet::MONDAY, '18:30:00.000' )->edit();
-            // case 3c: every Tuesday at 3 am
-            /*
-            $s->setScheduledPublishing( 
-                true, a\PublishSet::TUESDAY, NULL, NULL, '03:00:00.000' )->edit();
-            */
-            // case 4a: every Tuesday and Thursday at 6:30 am
-            /*
-            $s->setScheduledPublishing( true, 
-                array( a\PublishSet::TUESDAY, a\PublishSet::THURSDAY ), 
-                NULL, NULL, '06:30:00.000' )->edit();
-            */
-            // case 4b: every Friday and Sunday at 1:00 am
-            /*
-            $s->setDayOfWeek( 
-                array( a\PublishSet::FRIDAY, a\PublishSet::SUNDAY ),
-                '01:00:00.000' )->edit();
-            */
-            // case 5a: everyday at 6:30 am, 
-            // intentionally having repeated weekdays and random order
-            /*
-            $s->setScheduledPublishing( true, 
-                array( a\PublishSet::FRIDAY, a\PublishSet::FRIDAY, 
-                    a\PublishSet::THURSDAY, a\PublishSet::SATURDAY, 
-                    a\PublishSet::SUNDAY, a\PublishSet::WEDNESDAY,
-                    a\PublishSet::TUESDAY, a\PublishSet::THURSDAY, 
-                    a\PublishSet::MONDAY, a\PublishSet::SUNDAY ), 
-                //NULL, NULL, '06:30:00.000' )->edit();
-                NULL, NULL, '01:00:00.000' )->edit();
-            */
-            // case 5b: everyday at 12 am
-            //$s->setDayOfWeek( $s->getDaysOfWeek(), '01:00:00.000' )->edit();
-            // case 6a: every 2 hours starting at 6:15 am
-            /*
-            $s->setScheduledPublishing( 
-                true, NULL, 2, NULL, '06:15:00.000' )->edit();
-            */
-            // case 6b: every 4 hours
-            //$s->setPublishIntervalHours( 4 )->edit();
-            // case 7a: cron expression
-            $s->setScheduledPublishing( 
-                true, NULL, NULL, "0 0 12 * * ?" )->edit();
+            echo $s->getRootAssetFactoryContainerId(), BR;
+            echo $s->getRootConnectorContainerId(), BR;
+            echo $s->getRootContentTypeContainerId(), BR;
+            echo $s->getRootDataDefinitionContainerId(), BR;
+            echo $s->getRootFolderId(), BR;
+            echo $s->getRootMetadataSetContainerId(), BR;
+            echo $s->getRootPageConfigurationSetContainerId(), BR;
+            echo $s->getRootPublishSetContainerId(), BR;
+            echo $s->getRootSiteDestinationContainerId(), BR;
+            echo $s->getRootTransportContainerId(), BR;
+            echo $s->getRootWorkflowDefinitionContainerId(), BR;
+            echo $s->getSiteAssetFactoryContainerId(), BR;
+            echo $s->getSiteAssetFactoryContainerPath(), BR;
             
-            // case 7b: cron expression
-            //$s->setCronExpression( "0 4 12 * * ?" )->edit();
+            echo u\StringUtility::getCoalescedString( $s->getSiteStartingPageId() ), BR;
+            echo u\StringUtility::getCoalescedString( $s->getSiteStartingPagePath() ), BR;
+            echo u\StringUtility::boolToString( $s->getSiteStartingPageRecycled() ), BR;
+            echo u\StringUtility::boolToString( $s->getUnpublishOnExpiration() ), BR;
+            echo $s->getUrl(), BR;
+            echo u\StringUtility::boolToString( $s->hasRole(
+                $cascade->getAsset( a\Role::TYPE, 5 ) ) ), BR;
+            
+            
+            echo u\StringUtility::getCoalescedString(
+                $s->getScheduledPublishDestinationMode() ), BR;
+            echo u\StringUtility::getCoalescedString(
+                $s->getScheduledPublishDestinations() ), BR;
+                
                 
             if( $mode != 'all' )
                 break;
@@ -150,12 +104,12 @@ try
                 setUrl( 'http://www.upstate.edu/tuw-test' )->
                 // metadata set
                 setDefaultMetadataSet( 
-                    a\Asset::getAsset( $service, a\MetadataSet::TYPE,
-                        'b893fd058b7f0856002a5e11185ff809' ) )->
+                    $cascade->getAsset( a\MetadataSet::TYPE,
+                        '1f22ac858b7ffe834c5fe91e67ea0fcf' ) )->
                 // css
                 setCssFile(
                 /*
-                    a\Asset::getAsset( $service, a\File::TYPE, 
+                    $cascade->getAsset( a\File::TYPE, 
                         '4007ae9d8b7f08560139425c99384b99' ), 
                         'leftobject,rightobject,center,centerobject'
                 */
@@ -163,29 +117,31 @@ try
                 )->
                 // asset factory container
                 setSiteAssetFactoryContainer( 
-                    a\Asset::getAsset( 
-                        $service, a\AssetFactoryContainer::TYPE,
-                        '04af3e2e8b7f085600e28886d7e2221d' )
+                    $cascade->getAsset( a\AssetFactoryContainer::TYPE,
+                        '1f217d838b7ffe834c5fe91e9832f910' )
                     // NULL
                 )->
                 // starting page
                 setStartingPage( 
-                    a\Asset::getAsset( 
-                        $service, a\Page::TYPE, 
-                        '04af1d7a8b7f085600e28886865346c3' )
+                    $cascade->getAsset( a\Page::TYPE, 
+                        '1f2376798b7ffe834c5fe91ead588ce1' )
                     //NULL
                 )->
                 // send report on error
                 setSendReportOnErrorOnly( true )->
                 // add user to send report
                 addUserToSendReport( a\Asset::getAsset( 
-                    $service, a\User::TYPE, 'tuw' ) )->
+                    $service, a\User::TYPE, 'wing' ) )->
                 // add group to send report
                 addGroupToSendReport( a\Asset::getAsset( 
-                    $service, a\Group::TYPE, 'hemonc' ) )->
+                    $service, a\Group::TYPE, 'Administrators' ) )->
                 // expiration
                 setRecycleBinExpiration( a\Site::NEVER )->
-                edit();
+                setExternalLinkCheckOnPublish( true )->
+                setLinkCheckerEnabled( false )->
+                setUnpublishOnExpiration( true )->
+                
+                edit()->dump();
                 
             if( $mode != 'all' )
                 break;
@@ -197,9 +153,41 @@ try
                 break;
         
         case 'asset-tree':
-            echo S_PRE . u\XMLUtility::replaceBrackets( 
+/*/
+            u\DebugUtility::dump( u\XMLUtility::replaceBrackets( 
                 $s->getRootAssetFactoryContainerAssetTree()->
-                toXml() ) . E_PRE;
+                toXml() ) );
+            u\DebugUtility::dump( u\XMLUtility::replaceBrackets( 
+                $s->getRootConnectorContainerAssetTree()->
+                toXml() ) );
+            u\DebugUtility::dump( u\XMLUtility::replaceBrackets( 
+                $s->getRootContentTypeContainerAssetTree()->
+                toXml() ) );
+            u\DebugUtility::dump( u\XMLUtility::replaceBrackets( 
+                $s->getRootDataDefinitionContainerAssetTree()->
+                toXml() ) );
+            u\DebugUtility::dump( u\XMLUtility::replaceBrackets( 
+                $s->getRootMetadataSetContainerAssetTree()->
+                toXml() ) );
+            u\DebugUtility::dump( u\XMLUtility::replaceBrackets( 
+                $s->getRootPageConfigurationSetContainerAssetTree()->
+                toXml() ) );
+            u\DebugUtility::dump( u\XMLUtility::replaceBrackets( 
+                $s->getRootPublishSetContainerAssetTree()->
+                toXml() ) );
+            u\DebugUtility::dump( u\XMLUtility::replaceBrackets( 
+                $s->getRootSiteDestinationContainerAssetTree()->
+                toXml() ) );
+            u\DebugUtility::dump( u\XMLUtility::replaceBrackets( 
+                $s->getRootTransportContainerAssetTree()->
+                toXml() ) );
+            u\DebugUtility::dump( u\XMLUtility::replaceBrackets( 
+                $s->getRootWorkflowDefinitionContainerAssetTree()->
+                toXml() ) );
+/*/
+            u\DebugUtility::dump( u\XMLUtility::replaceBrackets( 
+                $s->getSiteAssetFactoryContainerAssetTree()->
+                toXml() ) );
                 
             if( $mode != 'all' )
                 break;
@@ -238,9 +226,15 @@ try
             if( $mode != 'all' )
                 break;
     }
+    
+    echo u\ReflectionUtility::getClassDocumentation( "cascade_ws_asset\Site" );
 }
 catch( \Exception $e )
 {
     echo S_PRE . $e . E_PRE;
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE;
 }
 ?>
