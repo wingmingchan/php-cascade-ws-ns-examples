bff6ccd4bbdf5feb11d90fccfeb5088fc91359a5
Wing Ming Chan <chanw@upstate.edu>
2016-12-19 10:56:15 -0500

    Updated

--- asset-class-test-code/user.php
+++ asset-class-test-code/user.php
@@ -1,22 +1,23 @@
 <?php
-require_once('cascade_ws_ns/auth_chanw.php');
+require_once('auth_tutorial7.php');
 
 use cascade_ws_constants as c;
-use cascade_ws_asset as a;
-use cascade_ws_property as p;
-use cascade_ws_utility as u;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
 
 $mode = 'all';
 //$mode = 'display';
 //$mode = 'dump';
-//$mode = 'get';
+$mode = 'get';
 //$mode = 'set';
 //$mode = 'raw';
+//$mode = 'none';
 
 try
 {
-    $id = 'chanw-test';
+    $id = 'wing';
     $u  = $cascade->getAsset( a\User::TYPE, $id );
     
     switch( $mode )
@@ -35,8 +36,26 @@ try
                 break;
                 
         case 'get':
-            echo "Username: " . $u->getUsername() . BR .
-                "Full name: " . $u->getFullName() . BR;
+            echo "Username: " . $u->getUsername(), BR .
+                 "Full name: " . u\StringUtility::getCoalescedString(
+                     $u->getFullName() ), BR;
+            echo $u->getAuthType(), BR;
+            echo u\StringUtility::getCoalescedString( $u->getDefaultGroup() ), BR;
+            echo u\StringUtility::getCoalescedString( $u->getDefaultSiteId() ), BR;
+            echo u\StringUtility::getCoalescedString( $u->getDefaultSiteName() ), BR;
+            echo u\StringUtility::boolToString( $u->getEnabled() ), BR;
+            echo $u->getEmail(), BR;
+            echo $u->getGroups(), BR;
+            echo $u->getRole(), BR;
+            echo $u->getPassword(), BR;
+            echo u\StringUtility::boolToString( $u->isInGroup(
+                $cascade->getAsset( a\Group::TYPE, "Administrators" )
+            ) ), BR;
+            
+            //$u->joinGroup( $cascade->getAsset( a\Group::TYPE, "cru" ) );
+            //$u->leaveGroup( $cascade->getAsset( a\Group::TYPE, "cru" ) );
+            $u->setDefaultGroup( $cascade->getAsset( a\Group::TYPE, "cru" ) )->edit();
+            $u->dump();
 
             if( $mode != 'all' )
                 break;
@@ -74,16 +93,20 @@ try
                 echo $service->getMessage();
             }
 */            
-            echo S_PRE;
-            var_dump( $user_std );
-            echo E_PRE;
+            u\DebugUtility::dump( $user_std );
        
             if( $mode != 'all' )
                 break;
     }
+
+    echo u\ReflectionUtility::getClassDocumentation( "cascade_ws_asset\TwitterConnector" );
 }
 catch( \Exception $e )
 {
     echo S_PRE . $e . E_PRE;
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE;
 }
 ?>
