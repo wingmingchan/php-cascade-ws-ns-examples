4e457b9a9ded418c461c9f4030f7ab1e150cf6c0
Wing Ming Chan <chanw@upstate.edu>
2016-12-19 10:58:41 -0500

    Updated

--- asset-class-test-code/workflow_definition.php
+++ asset-class-test-code/workflow_definition.php
@@ -1,23 +1,24 @@
 <?php
-require_once('cascade_ws_ns/auth_chanw.php');
+require_once('auth_tutorial7.php');
 
 use cascade_ws_constants as c;
-use cascade_ws_asset as a;
-use cascade_ws_property as p;
-use cascade_ws_utility as u;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
 
 $mode = 'all';
 //$mode = 'display';
-//$mode = 'dump';
-//$mode = 'get';
-//$mode = 'set';
+$mode = 'dump';
+$mode = 'get';
+$mode = 'set';
 //$mode = 'raw';
+//$mode = 'none';
 
 try
 {
-    $id  = "65221ba58b7f085600ae22825f36b20e"; // Test Workflow
-    $wfd = $cascade->getAsset( a\WorkflowDefinition::TYPE, $id );
+    $id  = "1f2421778b7ffe834c5fe91e46bef804"; // Test Workflow
+    $wfd = $cascade->getAsset( a\WorkflowDefinition::TYPE, $id )->dump() ;
     
     switch( $mode )
     {
@@ -38,25 +39,46 @@ try
         case 'get':
             echo "ID: " . $wfd->getId() . BR .
                  "Name: " . $wfd->getName() . BR .
-                 "Copy: " . ( $wfd->getCopy() ? 'true' : 'false' ) . BR .
-                 "Create: " . ( $wfd->getCreate() ? 'true' : 'false' ) . BR .
-                 "Delete: " . ( $wfd->getDelete() ? 'true' : 'false' ) . BR .
-                 "Edit: " . ( $wfd->getEdit() ? 'true' : 'false' ) . BR .
+                 
+                 
+                 "Copy: " . u\StringUtility::boolToString( $wfd->getCopy() ), BR,
+                 "Create: " . u\StringUtility::boolToString( $wfd->getCreate() ), BR,
+                 "Delete: " . u\StringUtility::boolToString( $wfd->getDelete() ), BR,
+                 "Edit: " . u\StringUtility::boolToString( $wfd->getEdit() ), BR,
                  "";
                  
-            echo S_PRE;
-            var_dump( $wfd->getOrderedStep( 'initialize' ) );
-            echo E_PRE;            
+            echo u\StringUtility::getCoalescedString( $wfd->getApplicableGroups() ), BR;
+            echo $wfd->getNamingBehavior(), BR;
+                 
+            /*
+            u\DebugUtility::dump( $wfd->getOrderedStep( 'initialize' ) );
+            u\DebugUtility::dump( $wfd->getOrderedSteps() );
+            u\DebugUtility::dump( $wfd->getNonOrderedStep( 'edit' ) );
+            u\DebugUtility::dump( $wfd->getNonOrderedSteps() );
+            u\DebugUtility::dump( u\XmlUtility::replaceBrackets( $wfd->getXml() ) );
+            */
+            
+            echo u\StringUtility::boolToString( $wfd->hasNonOrderedStep( 'edit' ) ), BR;
+            echo u\StringUtility::boolToString( $wfd->hasOrderedStep( 'initialize' ) ), BR;
+            echo u\StringUtility::boolToString( $wfd->isApplicableToGroup(
+                $cascade->getAsset( a\Group::TYPE, "Administrators" )
+            ) ), BR;
 
             if( $mode != 'all' )
                 break;
              
         case 'set':
             $wfd->setNamingBehavior( 
-                a\WorkflowDefinition::NAMING_BEHAVIOR_DEFINITION )->
+                    a\WorkflowDefinition::NAMING_BEHAVIOR_DEFINITION )->
                 setEdit( false )->
                 addGroup( $cascade->getAsset( a\Group::TYPE, 'demo' ) )->
-                edit();
+                setCopy( true )->
+                setCreate( true )->
+                setDelete( true )->
+                setEdit( true )->
+                
+                
+                edit()->dump();
                 
             if( $mode != 'all' )
                 break;
@@ -65,16 +87,22 @@ try
             $wfd_std = $service->retrieve( 
                 $service->createId( 
                     c\T::WORKFLOWDEFINITION, $id ), c\P::WORKFLOWDEFINITION );
-            echo S_PRE;
-            var_dump( $wfd_std );
-            echo E_PRE;
+
+            u\DebugUtility::dump( $wfd_std );
+
        
             if( $mode != 'all' )
                 break;
     }
+
+    echo u\ReflectionUtility::getClassDocumentation( "cascade_ws_asset\WorkflowDefinition" );
 }
 catch( \Exception $e )
 {
     echo S_PRE . $e . E_PRE;
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE;
 }
 ?>
