7d6c33297cb4a1508c1a49000b62fff696f14c32
Wing Ming Chan <chanw@upstate.edu>
2016-12-19 10:17:51 -0500

    Updated

--- asset-class-test-code/role.php
+++ asset-class-test-code/role.php
@@ -1,24 +1,30 @@
 <?php
-require_once('cascade_ws_ns/auth_chanw.php');
+//require_once('auth_tutorial7.php');
+require_once('auth_chanw.php');
 
 use cascade_ws_constants as c;
-use cascade_ws_asset as a;
-use cascade_ws_property as p;
-use cascade_ws_utility as u;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
 
 $mode = 'all';
 //$mode = 'display';
 //$mode = 'dump';
 //$mode = 'get';
-//$mode = 'set-global';
+$mode = 'set-global';
 //$mode = 'set-site';
 //$mode = 'raw';
+$mode = 'none';
 
 try
 {
+    //$r = $cascade->getAsset( a\Role::TYPE, 210 )->dump();
+    //$r = $cascade->getRoleAssetByName( "Account Manager" )->dump();
+    u\DebugUtility::dump( $cascade->getRoles() );
+
     $r = $cascade->getAsset( a\Role::TYPE, 110 ); // global role
-    //$r = $cascade->getAsset( a\Role::TYPE, 55 ); // site role
+    //$r = $cascade->getAsset( a\Role::TYPE, 210 ); // site role
     
     switch( $mode )
     {
@@ -66,89 +72,89 @@ try
             {
                 $ga->
                     // methods from parent
-                    setAccessAdminArea( true )->
-                    setAccessAssetFactories( true )->
-                    setAccessAudits( true )->
-                    setAccessConfigurationSets( true )->
-                    setAccessContentTypes( true )->
-                    setAccessDataDefinitions( true )->
-                    setAccessMetadataSets( true )->
-                    setAccessPublishSets( true )->
-                    setAccessTransports( true )->
-                    setAccessWorkflowDefinitions( true )->
-                    setActivateDeleteVersions( true )->
-                    setAlwaysAllowedToToggleDataChecks( true )->
-                    setAssignApproveWorkflowSteps( true )->
-                    setBulkChange( true )->
-                    setBypassWorkflow( true )->
-                    setBreakLocks( true )->
-                    setAssignWorkflowsToFolders( true )->
-                    setBypassAssetFactoryGroupsNewMenu( true )->
-                    setBypassDestinationGroupsWhenPublishing( true )->
-                    setBypassWorkflowDefintionGroupsForFolders( true )->
-                    setBypassAllPermissionsChecks( true )->
-                    setCancelPublishJobs( true )->
-                    setDeleteWorkflows( true )->
-                    setDiagnosticTests( true )->
-                    setEditAccessRights( true )->
-                    setEditDataDefinition( true )->
-                    setEditPageContentType( true )->
-                    setEditPageLevelConfigurations( true )->
-                    setIntegrateFolder( true )->
-                    setImportZipArchive( true )->
-                    setMoveRenameAssets( true )->
-                    setMultiSelectCopy( true )->
-                    setMultiSelectDelete( true )->
-                    setMultiSelectMove( true )->
-                    setMultiSelectPublish( true )->
-                    setPublishReadableAdminAreaAssets( true )->
-                    setPublishReadableHomeAssets( true )->
-                    setPublishWritableAdminAreaAssets( true )->
-                    setPublishWritableHomeAssets( true )->
-                    setRecycleBinDeleteAssets( true )->
-                    setRecycleBinViewRestoreAllAssets( true )->
-                    setRecycleBinViewRestoreUserAssets( true )->
-                    setReorderPublishQueue( true )->
-                    setSendStaleAssetNotifications( true )->
-                    setUploadImagesFromWysiwyg( true )->
-                    setViewPublishQueue( true )->
-                    setViewVersions( true )->
+                    setAccessAdminArea( false )->
+                    setAccessAssetFactories( false )->
+                    setAccessAudits( false )->
+                    setAccessConfigurationSets( false )->
+                    setAccessContentTypes( false )->
+                    setAccessDataDefinitions( false )->
+                    setAccessMetadataSets( false )->
+                    setAccessPublishSets( false )->
+                    setAccessTransports( false )->
+                    setAccessWorkflowDefinitions( false )->
+                    setActivateDeleteVersions( false )->
+                    setAlwaysAllowedToToggleDataChecks( false )->
+                    setAssignApproveWorkflowSteps( false )->
+                    setBulkChange( false )->
+                    setBypassWorkflow( false )->
+                    setBreakLocks( false )->
+                    setAssignWorkflowsToFolders( false )->
+                    setBypassAssetFactoryGroupsNewMenu( false )->
+                    setBypassDestinationGroupsWhenPublishing( false )->
+                    setBypassWorkflowDefintionGroupsForFolders( false )->
+                    setBypassAllPermissionsChecks( false )->
+                    setCancelPublishJobs( false )->
+                    setDeleteWorkflows( false )->
+                    setDiagnosticTests( false )->
+                    setEditAccessRights( false )->
+                    setEditDataDefinition( false )->
+                    setEditPageContentType( false )->
+                    setEditPageLevelConfigurations( false )->
+                    setIntegrateFolder( false )->
+                    setImportZipArchive( false )->
+                    setMoveRenameAssets( false )->
+                    setMultiSelectCopy( false )->
+                    setMultiSelectDelete( false )->
+                    setMultiSelectMove( false )->
+                    setMultiSelectPublish( false )->
+                    setPublishReadableAdminAreaAssets( false )->
+                    setPublishReadableHomeAssets( false )->
+                    setPublishWritableAdminAreaAssets( false )->
+                    setPublishWritableHomeAssets( false )->
+                    setRecycleBinDeleteAssets( false )->
+                    setRecycleBinViewRestoreAllAssets( false )->
+                    setRecycleBinViewRestoreUserAssets( false )->
+                    setReorderPublishQueue( false )->
+                    setSendStaleAssetNotifications( false )->
+                    setUploadImagesFromWysiwyg( false )->
+                    setViewPublishQueue( false )->
+                    setViewVersions( false )->
                     
                     // method from class
-                    setAccessAllSites( true )->
-                    setAccessConfiguration( true )->
-                    setAccessRoles( true )->
-                    setAccessSecurityArea( true )->
-                    setAccessSiteManagement( true )->
-                    setAccessTargetsDestinations( true )->
-                    setBroadcastMessages( true )->
-                    setConfigureLogging( true )->
-                    setCreateGroups( true )->
-                    setCreateRoles( true )->
-                    setCreateSites( true )->
-                    setCreateUsers( true )->
-                    setDatabaseExportTool( true )->
-                    setDeleteAllUsers( true )->
-                    setDeleteAnyGroup( true )->
-                    setDeleteMemberGroups( true )->
-                    setDeleteUsersInMemberGroups( true )->
-                    setEditAnyGroup( true )->
-                    setEditAnyUser( true )->
-                    setEditMemberGroups( true )->
-                    setEditSystemPreferences( true )->
-                    setEditUsersInMemberGroups( true )->
-                    setForceLogout( true )->
-                    setNewSiteWizard( true )->
-                    setOptimizeDatabase( true )->
-                    setPathRepairTool( true )->
-                    setRecycleBinChecker( true )->
-                    setSearchingIndexing( true )->
-                    setSyncLdap( true )->
-                    setViewSystemInfoAndLogs( true )->
-                    setSiteMigration( true )->
-                    setViewAllGroups( true )->
-                    setViewAllUsers( true )->
-                    setViewMemberGroups( true )->
+                    setAccessAllSites( false )->
+                    setAccessConfiguration( false )->
+                    setAccessRoles( false )->
+                    setAccessSecurityArea( false )->
+                    setAccessSiteManagement( false )->
+                    setAccessTargetsDestinations( false )->
+                    setBroadcastMessages( false )->
+                    setConfigureLogging( false )->
+                    setCreateGroups( false )->
+                    setCreateRoles( false )->
+                    setCreateSites( false )->
+                    setCreateUsers( false )->
+                    setDatabaseExportTool( false )->
+                    setDeleteAllUsers( false )->
+                    setDeleteAnyGroup( false )->
+                    setDeleteMemberGroups( false )->
+                    setDeleteUsersInMemberGroups( false )->
+                    setEditAnyGroup( false )->
+                    setEditAnyUser( false )->
+                    setEditMemberGroups( false )->
+                    setEditSystemPreferences( false )->
+                    setEditUsersInMemberGroups( false )->
+                    setForceLogout( false )->
+                    setNewSiteWizard( false )->
+                    setOptimizeDatabase( false )->
+                    setPathRepairTool( false )->
+                    setRecycleBinChecker( false )->
+                    setSearchingIndexing( false )->
+                    setSyncLdap( false )->
+                    setViewSystemInfoAndLogs( false )->
+                    setSiteMigration( false )->
+                    setViewAllGroups( false )->
+                    setViewAllUsers( false )->
+                    setViewMemberGroups( false )->
                     setViewUsersInMemberGroups( true );
                 
                 $r->edit();
@@ -164,56 +170,56 @@ try
             {
                 $sa->
                     // methods from parent
-                    setAccessAdminArea( true )->
-                    setAccessAssetFactories( true )->
-                    setAccessAudits( true )->
-                    setAccessConfigurationSets( true )->
-                    setAccessContentTypes( true )->
-                    setAccessDataDefinitions( true )->
-                    setAccessMetadataSets( true )->
-                    setAccessPublishSets( true )->
-                    setAccessTransports( true )->
-                    setAccessWorkflowDefinitions( true )->
-                    setActivateDeleteVersions( true )->
-                    setAlwaysAllowedToToggleDataChecks( true )->
-                    setAssignApproveWorkflowSteps( true )->
-                    setBulkChange( true )->
-                    setBypassWorkflow( true )->
-                    setBreakLocks( true )->
-                    setAssignWorkflowsToFolders( true )->
-                    setBypassAssetFactoryGroupsNewMenu( true )->
-                    setBypassDestinationGroupsWhenPublishing( true )->
-                    setBypassWorkflowDefintionGroupsForFolders( true )->
-                    setBypassAllPermissionsChecks( true )->
-                    setCancelPublishJobs( true )->
-                    setDeleteWorkflows( true )->
-                    setDiagnosticTests( true )->
-                    setEditAccessRights( true )->
-                    setEditDataDefinition( true )->
-                    setEditPageContentType( true )->
-                    setEditPageLevelConfigurations( true )->
-                    setIntegrateFolder( true )->
-                    setImportZipArchive( true )->
-                    setMoveRenameAssets( true )->
-                    setMultiSelectCopy( true )->
-                    setMultiSelectDelete( true )->
-                    setMultiSelectMove( true )->
-                    setMultiSelectPublish( true )->
-                    setPublishReadableAdminAreaAssets( true )->
-                    setPublishReadableHomeAssets( true )->
-                    setPublishWritableAdminAreaAssets( true )->
-                    setPublishWritableHomeAssets( true )->
-                    setRecycleBinDeleteAssets( true )->
-                    setRecycleBinViewRestoreAllAssets( true )->
-                    setRecycleBinViewRestoreUserAssets( true )->
-                    setReorderPublishQueue( true )->
-                    setSendStaleAssetNotifications( true )->
-                    setUploadImagesFromWysiwyg( true )->
-                    setViewPublishQueue( true )->
-                    setViewVersions( true )->
+                    setAccessAdminArea( false )->
+                    setAccessAssetFactories( false )->
+                    setAccessAudits( false )->
+                    setAccessConfigurationSets( false )->
+                    setAccessContentTypes( false )->
+                    setAccessDataDefinitions( false )->
+                    setAccessMetadataSets( false )->
+                    setAccessPublishSets( false )->
+                    setAccessTransports( false )->
+                    setAccessWorkflowDefinitions( false )->
+                    setActivateDeleteVersions( false )->
+                    setAlwaysAllowedToToggleDataChecks( false )->
+                    setAssignApproveWorkflowSteps( false )->
+                    setBulkChange( false )->
+                    setBypassWorkflow( false )->
+                    setBreakLocks( false )->
+                    setAssignWorkflowsToFolders( false )->
+                    setBypassAssetFactoryGroupsNewMenu( false )->
+                    setBypassDestinationGroupsWhenPublishing( false )->
+                    setBypassWorkflowDefintionGroupsForFolders( false )->
+                    setBypassAllPermissionsChecks( false )->
+                    setCancelPublishJobs( false )->
+                    setDeleteWorkflows( false )->
+                    setDiagnosticTests( false )->
+                    setEditAccessRights( false )->
+                    setEditDataDefinition( false )->
+                    setEditPageContentType( false )->
+                    setEditPageLevelConfigurations( false )->
+                    setIntegrateFolder( false )->
+                    setImportZipArchive( false )->
+                    setMoveRenameAssets( false )->
+                    setMultiSelectCopy( false )->
+                    setMultiSelectDelete( false )->
+                    setMultiSelectMove( false )->
+                    setMultiSelectPublish( false )->
+                    setPublishReadableAdminAreaAssets( false )->
+                    setPublishReadableHomeAssets( false )->
+                    setPublishWritableAdminAreaAssets( false )->
+                    setPublishWritableHomeAssets( false )->
+                    setRecycleBinDeleteAssets( false )->
+                    setRecycleBinViewRestoreAllAssets( false )->
+                    setRecycleBinViewRestoreUserAssets( false )->
+                    setReorderPublishQueue( false )->
+                    setSendStaleAssetNotifications( false )->
+                    setUploadImagesFromWysiwyg( false )->
+                    setViewPublishQueue( false )->
+                    setViewVersions( false )->
                     
                     // method from class
-                    setAccessConnectors( true )->
+                    setAccessConnectors( false )->
                     setAccessDestinations( true );
                 $r->edit();
             }
@@ -244,16 +250,20 @@ try
                 c\T::ROLE, 110 ), c\P::ROLE );
 */
                 
-            echo S_PRE;
-            var_dump( $r_std );
-            echo E_PRE;
+            u\DebugUtility::dump( $r_std );
        
             if( $mode != 'all' )
                 break;
     }
+    
+    echo u\ReflectionUtility::getClassDocumentation( "cascade_ws_asset\Role" );
 }
 catch( \Exception $e )
 {
     echo S_PRE . $e . E_PRE;
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE;
 }
 ?>
