d8ea012c1e0139cc8d163e28428873724af1e3fb
Wing Ming Chan <chanw@upstate.edu>
2016-12-30 06:49:11 -0500

    Updated

--- recipes/asset_factory/move_synch_asset_factories.php
+++ recipes/asset_factory/move_synch_asset_factories.php
@@ -11,36 +11,38 @@ use cascade_ws_exception as e;
 
 try
 {
-	$site_names = array(
-		"database-test",
-	);
-	
-	foreach( $site_names as $site_name )
-	{
-		$root_af_container = $cascade->getSite( $site_name )->getRootAssetFactoryContainer();
-		$create_container  = $cascade->getAssetFactoryContainer( "Create", $site_name );
-		$upload_container  = $cascade->getAssetFactoryContainer( "Upload", $site_name );
-		
-		if( !isset( $create_container ) )
-		{
-			// create asset factory container
-    		$create_container = $cascade->createAssetFactoryContainer(
-        		$root_af_container,
-            	"Create"
-    		);
-		}
-		
-		if( !isset( $upload_container ) )
-		{
-			// create asset factory container
-    		$create_container = $cascade->createAssetFactoryContainer(
-        		$root_af_container,
-            	"Upload"
-    		);
-		}
-		
-		
-	}
+    $site_names = array(
+        "database-test",
+    );
+    
+    foreach( $site_names as $site_name )
+    {
+        $root_af_container = $cascade->getSite(
+            $site_name )->getRootAssetFactoryContainer();
+        $create_container  = $cascade->getAssetFactoryContainer( "Create", $site_name );
+        $upload_container  = $cascade->getAssetFactoryContainer( "Upload", $site_name );
+        
+        if( !isset( $create_container ) )
+        {
+            // create asset factory container
+            $create_container = $cascade->createAssetFactoryContainer(
+                $root_af_container,
+                "Create"
+            );
+        }
+        
+        if( !isset( $upload_container ) )
+        {
+            // create asset factory container
+            $create_container = $cascade->createAssetFactoryContainer(
+                $root_af_container,
+                "Upload"
+            );
+        }
+        
+        
+        
+    }
 }
 catch( \Exception $e ) 
 {
