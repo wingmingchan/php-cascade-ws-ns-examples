49d7fbdd76667e726e2f089dde9d6ae25ff20ab9
Wing Ming Chan <chanw@upstate.edu>
2016-12-30 11:34:07 -0500

    Updated

--- recipes/asset_factory/report_asset_factory_group_assignment.php
+++ recipes/asset_factory/report_asset_factory_group_assignment.php
@@ -1,5 +1,7 @@
 <?php 
-require_once('cascade_ws_ns/auth_chanw.php');
+$start_time = time();
+
+require_once('auth_chanw.php');
 
 use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
@@ -8,15 +10,10 @@ use cascade_ws_property  as p;
 use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
 
-$start_time = time();
+u\DebugUtility::setTimeSpaceLimits();
 
 try
 {
-    // to prevent time-out
-    set_time_limit ( 10000 );
-    // to prevent using up memory when traversing a large site
-    ini_set( 'memory_limit', '2048M' );
-    
     // get the sites
     $sites   = $cascade->getSites();
     $results = array();
@@ -49,13 +46,16 @@ try
         echo "</ul>\n";
     }
     
-    $end_time = time();
-    echo "\nTotal time taken: " . ( $end_time - $start_time ) . " seconds\n";
+    u\DebugUtility::outputDuration( $start_time );
 }
 catch( \Exception $e ) 
 {
     echo S_PRE . $e . E_PRE;
-    $end_time = time();
-    echo "\nTotal time taken: " . ( $end_time - $start_time ) . " seconds\n";
+    u\DebugUtility::outputDuration( $start_time );
+}
+catch( \Error $er ) 
+{
+    echo S_PRE . $er . E_PRE;
+    u\DebugUtility::outputDuration( $start_time );
 }
 ?>
