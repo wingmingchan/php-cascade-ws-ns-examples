e0616a7a6679c7559556014db77f5ff49fa937ca
Wing Ming Chan <chanw@upstate.edu>
2017-01-03 14:04:23 -0500

    Updated

--- recipes/asset_tree_traversal/use_global_function.php
+++ recipes/asset_tree_traversal/use_global_function.php
@@ -1,5 +1,9 @@
 <?php 
-require_once('cascade_ws_ns/auth_chanw.php');
+/*
+This program shows how a global function, designed for tree traversal,
+can be called directly for testing purposes.
+*/
+require_once('auth_chanw.php');
 
 use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
@@ -21,6 +25,11 @@ catch( \Exception $e )
 {
     echo S_PRE . $e . E_PRE; 
 }
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE;
+}
+
 
 function assetTreeSimpleFunction(
     aohs\AssetOperationHandlerService $service, 
