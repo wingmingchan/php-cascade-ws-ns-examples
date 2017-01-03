dc0857cd2c4e9cd3a0dd1bc4617d629553bd20a3
Wing Ming Chan <chanw@upstate.edu>
2017-01-03 13:11:18 -0500

    Updated

--- recipes/asset_tree_traversal/report_server_file_orphan.php
+++ recipes/asset_tree_traversal/report_server_file_orphan.php
@@ -1,5 +1,5 @@
 <?php
-require_once('cascade_ws_ns/auth_chanw.php');
+require_once('auth_chanw.php');
 
 use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
@@ -60,6 +60,10 @@ try
 catch( \Exception $e ) 
 {
     echo S_PRE . $e . E_PRE;
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE;
 }
 
 function processFolder( $dir, &$array ) 
