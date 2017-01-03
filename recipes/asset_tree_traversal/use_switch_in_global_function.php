7c6c2f0d3f98a8980ac7a4310936bbf5ad19b252
Wing Ming Chan <chanw@upstate.edu>
2017-01-03 14:21:03 -0500

    Updated

--- recipes/asset_tree_traversal/use_switch_in_global_function.php
+++ recipes/asset_tree_traversal/use_switch_in_global_function.php
@@ -1,5 +1,9 @@
 <?php 
-require_once('cascade_ws_ns/auth_chanw.php');
+/*
+This program shows how to use a switch statement in a global function to
+control the logic of processing assets of different types.
+*/
+require_once('auth_chanw.php');
 
 use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
@@ -22,6 +26,10 @@ try
 catch( \Exception $e ) 
 {
     echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE;
 }
 
 function assetTreeSimpleFunction(
@@ -46,6 +54,7 @@ function assetTreeSimpleFunction(
             break;
             
         default:
+        	echo "Encountered a $type. ";
             echo "Do Nothing", BR;
             break;
     }
