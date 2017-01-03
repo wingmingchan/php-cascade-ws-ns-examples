be72afc1cc3f8a75bba82dd080ebb414e8e4dedf
Wing Ming Chan <chanw@upstate.edu>
2017-01-03 08:09:58 -0500

    Updated

--- recipes/asset_tree_traversal/count_assets.php
+++ recipes/asset_tree_traversal/count_assets.php
@@ -4,21 +4,18 @@ This program is used to count assets of various types.
 */
 $start_time = time();
 
-require_once('cascade_ws_ns/auth_wing.php');
+require_once('auth_chanw.php');
 
 use cascade_ws_constants as c;
-use cascade_ws_AOHS as aohs;
-use cascade_ws_asset as a;
-use cascade_ws_property as p;
-use cascade_ws_utility as u;
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
 
 try
 {
-    // to prevent time-out
-    set_time_limit ( 10000 );
-    // to prevent using up memory when traversing a large site
-    ini_set( 'memory_limit', '2048M' );
+    u\DebugUtility::setTimeSpaceLimits();
     
     $site_name = 'cascade-admin';
 
@@ -45,13 +42,16 @@ try
 
     u\DebugUtility::dump( $results );
 
-    $end_time = time();
-    echo "\nTotal time taken: " . ( $end_time - $start_time ) . " seconds\n";
+    u\DebugUtility::outputDuration( $start_time );
 }
 catch( \Exception $e )
 {
     echo S_PRE . $e . E_PRE;
-    $end_time = time();
-    echo "\nTotal time taken: " . ( $end_time - $start_time ) . " seconds\n";
+    u\DebugUtility::outputDuration( $start_time );
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE;
+    u\DebugUtility::outputDuration( $start_time );
 }
 ?>
