8d9a2a73bc9d55a224353df8e6c895e3345e16c0
Wing Ming Chan <chanw@upstate.edu>
2017-01-03 14:12:35 -0500

    Updated

--- recipes/asset_tree_traversal/use_return_in_global_function.php
+++ recipes/asset_tree_traversal/use_return_in_global_function.php
@@ -1,5 +1,9 @@
 <?php 
-require_once('cascade_ws_ns/auth_chanw.php');
+/*
+This program shows how to use a return statement in a global function to skip
+processing assets.
+*/
+require_once('auth_chanw.php');
 
 use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
@@ -10,8 +14,11 @@ use cascade_ws_exception as e;
 
 try
 {
-    $cascade->getAsset( a\Folder::TYPE, "ffe39a278b7f08ee3e513744c5d70ead" )->getAssetTree()->
+    $cascade->getAsset(
+            a\Folder::TYPE, "ffe39a278b7f08ee3e513744c5d70ead" )->getAssetTree()->
         traverse(
+            // invoke the global function for four types of assets
+            // could be by mistake
             array( a\Page::TYPE =>         array( "assetTreeSimpleFunction" ),
                    a\Folder::TYPE =>       array( "assetTreeSimpleFunction" ),
                    a\ScriptFormat::TYPE => array( "assetTreeSimpleFunction" ),
@@ -23,6 +30,10 @@ catch( \Exception $e )
 {
     echo S_PRE . $e . E_PRE; 
 }
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE;
+}
 
 function assetTreeSimpleFunction(
     aohs\AssetOperationHandlerService $service, 
@@ -31,6 +42,8 @@ function assetTreeSimpleFunction(
 {
     $type = $child->getType();
     
+    // only process two types of assets
+    // skip all other types if the function is not designed to process them
     if( $type != a\Page::TYPE && $type != a\Folder::TYPE )
         return;
         
