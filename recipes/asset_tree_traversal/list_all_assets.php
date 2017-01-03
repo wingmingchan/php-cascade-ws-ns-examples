c89ac1959aedeb701b911493ee1aa3ff31e80b7c
Wing Ming Chan <chanw@upstate.edu>
2017-01-03 08:37:58 -0500

    Updated

--- recipes/asset_tree_traversal/list_all_assets.php
+++ recipes/asset_tree_traversal/list_all_assets.php
@@ -1,5 +1,5 @@
 <?php 
-require_once('cascade_ws_ns/auth_chanw.php');
+require_once('auth_chanw.php');
 
 use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
@@ -10,15 +10,18 @@ use cascade_ws_exception as e;
 
 try
 {
-    $at = $cascade->getAsset( a\Folder::TYPE, '4a4fc2d38b7f085600ebf23e49dfc2fd' )->
+    $at = $cascade->getAsset( a\Folder::TYPE, 'c0a77f3d8b7f0856002a5e11f1d4c763' )->
         getAssetTree();
     echo $at->toListString();
       
-    $xml_string = str_replace( "<", "&lt;", $at->toXml() );  
-    echo S_PRE, $xml_string, E_PRE;
+    echo S_PRE, u\XmlUtility::replaceBrackets( $at->toXml() ), E_PRE;  
 }
 catch( \Exception $e ) 
 {
     echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE;
 }
 ?>
