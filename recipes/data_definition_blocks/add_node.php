2a5f402d5e81d1153cfee930af67adcf3895d7ac
Wing Ming Chan <chanw@upstate.edu>
2017-01-04 12:54:53 -0500

    Updated

--- recipes/data_definition_blocks/add_node.php
+++ recipes/data_definition_blocks/add_node.php
@@ -1,5 +1,9 @@
 <?php
-require_once('cascade_ws_ns/auth_chanw.php');
+/*
+This program shows how to add a node to a multiple node set (defined as a multiple
+field in the data definition).
+*/
+require_once('auth_chanw.php');
 
 use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
@@ -17,5 +21,9 @@ try
 catch( \Exception $e ) 
 {
     echo S_PRE . $e . E_PRE;
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE;
 }
 ?>
