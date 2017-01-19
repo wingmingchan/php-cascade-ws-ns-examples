80bf7fa4e240e67a35f91852bf8e2867b7f4b070
Wing Ming Chan <chanw@upstate.edu>
2017-01-19 08:09:12 -0500

    Initial commit

---
+++ tutorial/introductory/1/get_asset.php
@@ -0,0 +1,24 @@
+<?php
+require_once( 'auth_tutorial7.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $page = $cascade->getAsset(
+        a\Page::TYPE, "54763ffe8b7ffe83552dce4f30290433" )->dump();
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
