bbad32ea82d5c6aa123bfec36d02861814ca74d1
Wing Ming Chan <chanw@upstate.edu>
2017-01-23 13:53:07 -0500

    Initial commit

---
+++ tutorial/introductory/4/publish_site.php
@@ -0,0 +1,25 @@
+<?php
+require_once( 'auth_chanw.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $site_name = "about";
+    $site = $cascade->getSite( $site_name );
+    $site->publish();
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
