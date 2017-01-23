80e67184b90d2788a06e055787380abab6736f68
Wing Ming Chan <chanw@upstate.edu>
2017-01-23 15:30:04 -0500

    Initial commit

---
+++ tutorial/introductory/4/publish_all_sites.php
@@ -0,0 +1,38 @@
+<?php
+require_once( 'auth_chanw.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    u\DebugUtility::dump( $service->createId( a\Site::TYPE, "cascade-admin" ) );
+
+    $sites = $cascade->getSites();
+    //$counter = 0;
+    
+    foreach( $sites as $site_id )
+    {
+        $service->publish( $site_id->toStdClass() );
+/*        
+        $counter++;
+        
+        if( $counter >= 1 )
+            break;
+*/
+    }
+
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
