1950dca7a883d25159a0af2eced4a3a2545f9d15
Wing Ming Chan <chanw@upstate.edu>
2017-01-23 13:43:41 -0500

    Initial commit

---
+++ tutorial/introductory/4/get_base_folder.php
@@ -0,0 +1,25 @@
+<?php
+require_once( 'auth_tutorial7.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $site_name = "cascade-admin";
+    $site = $cascade->getSite( $site_name );
+    $site_base_folder = $site->getBaseFolder()->dump();
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
