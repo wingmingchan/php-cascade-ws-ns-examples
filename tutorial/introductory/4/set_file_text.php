48674fc486b8e2e50f7dabc647e99a39b873177e
Wing Ming Chan <chanw@upstate.edu>
2017-01-23 15:56:10 -0500

    Initial commit

---
+++ tutorial/introductory/4/set_file_text.php
@@ -0,0 +1,25 @@
+<?php
+require_once( 'auth_tutorial7.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $file = $cascade->getAsset( a\File::TYPE, "081f5b248b7ffe8339ce5d137fcdb3f8" );
+    //$file->setText( "body{color:#ffffff}" )->edit();
+    $file->setData( "body{color:#ffffff}" )->edit();
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
