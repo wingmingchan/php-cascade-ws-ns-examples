519a3984ccfac351d5b0c62e462e6e0bb10da9ae
Wing Ming Chan <chanw@upstate.edu>
2017-01-23 14:11:16 -0500

    Initial commit

---
+++ tutorial/introductory/4/manipulate_site.php
@@ -0,0 +1,41 @@
+<?php
+require_once( 'auth_tutorial7.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $site_name = "cascade-admin-webapp";
+    $site = $cascade->getSite( $site_name );
+
+    $site->setCssFile(
+        // CSS file
+        $cascade->getAsset( a\File::TYPE,  '081d805b8b7ffe8339ce5d1303b53a50' ),
+        // CSS classes
+        'leftobject,rightobject,center,centerobject' )->
+        // turn on link check
+        setLinkCheckerEnabled( true )->
+        // external links
+        setExternalLinkCheckOnPublish( true )->
+        // recycle bin
+        setRecycleBinExpiration( a\Site::NEVER )->
+        // starting page
+        setStartingPage( 
+            $cascade->getAsset( a\Page::TYPE, 
+                '1f2376798b7ffe834c5fe91ead588ce1' ) )->
+        edit();
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
