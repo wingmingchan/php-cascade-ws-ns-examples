ce1ce1f4496e13e1b246ff048ba5d2ee9cbc8643
Wing Ming Chan <chanw@upstate.edu>
2017-01-26 09:11:21 -0500

    Initial commit

---
+++ recipes/pages/rename_page.php
@@ -0,0 +1,29 @@
+<?php
+/*
+This program shows how to rename a page.
+*/
+require_once( 'auth_chanw.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $page = $cascade->getAsset( a\Page::TYPE, '2a47653d8b7f08ee3c48c4e996f9054a' );
+    
+    // remove the last instance of a multiple field
+    $page->rename( "dummy-page" );
+}
+catch( \Exception $e )
+{
+    echo S_PRE . $e . E_PRE;
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
