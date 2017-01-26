06fd59ad895651c28c83a283586a2f3073975271
Wing Ming Chan <chanw@upstate.edu>
2017-01-26 10:16:22 -0500

    Updated

--- recipes/pages/set_shouldBePublished_shouldBeIndexed.php
+++ recipes/pages/set_shouldBePublished_shouldBeIndexed.php
@@ -1,5 +1,8 @@
 <?php 
-require_once('cascade_ws_ns/auth_chanw.php');
+/*
+This program shows how to attach a block to a region.
+*/
+require_once( 'auth_chanw.php' );
 
 use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
@@ -11,11 +14,15 @@ use cascade_ws_exception as e;
 try
 {
     $page = $cascade->getAsset( a\Page::TYPE, '2a47653d8b7f08ee3c48c4e996f9054a' );
-    $page->setShouldBePublished( false )->
-        setShouldBeIndexed( false )->edit();
+    $page->setShouldBePublished( true )->
+        setShouldBeIndexed( true )->edit();
 }
 catch( \Exception $e ) 
 {
     echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
 }
 ?>
