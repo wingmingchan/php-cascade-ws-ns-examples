2c268416ec3a5c9750e315ab6d606eb530754855
Wing Ming Chan <chanw@upstate.edu>
2017-01-26 10:05:39 -0500

    Updated

--- recipes/pages/set_region_format.php
+++ recipes/pages/set_region_format.php
@@ -1,5 +1,8 @@
 <?php 
-require_once('cascade_ws_ns/auth_chanw.php');
+/*
+This program shows how to attach a format to a region.
+*/
+require_once( 'auth_chanw.php' );
 
 use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
@@ -23,5 +26,9 @@ try
 catch( \Exception $e ) 
 {
     echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
 }
 ?>
