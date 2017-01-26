f84bd14c261f33ab0a8279d43fd4ff299fc8e488
Wing Ming Chan <chanw@upstate.edu>
2017-01-26 09:29:13 -0500

    Updated

--- recipes/pages/replace_text.php
+++ recipes/pages/replace_text.php
@@ -1,16 +1,21 @@
 <?php
-require_once('cascade_ws_ns/auth_chanw.php');
+/*
+This program shows how to replace a string with another string.
+Note that this assumes a case-sensitive string match.
+*/
+require_once( 'auth_chanw.php' );
 
+use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
-use cascade_ws_asset as a;
-use cascade_ws_property as p;
-use cascade_ws_utility as u;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
 
 try
 {
-    $site_name = "cascade-admin";
-    $page_name = "my_test_page";
+    $site_name = "cascade-admin-old";
+    $page_name = "test/velocity/dummy-page";
     $page      = $cascade->getAsset( a\Page::TYPE, $page_name, $site_name );
     $node_ids  = $page->getIdentifiers(); // the fully qualified identifiers
     
@@ -18,10 +23,11 @@ try
     {
         if( $page->isWYSIWYG( $id ) ) // only modify text in WYSIWYG's
         {
+        	echo $id, BR;
             $page->replaceText( 
-                "http://web.upstate.edu",
-                "http://www.upstate.edu",
-                array( $id )
+                "http://web.upstate.edu", // search
+                "http://www.upstate.edu", // replacement
+                array( $id )              // apply to this id only
             );
         }
     }
@@ -30,5 +36,9 @@ try
 catch( \Exception $e )
 {
     echo S_PRE . $e . E_PRE;
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
 }
 ?>
