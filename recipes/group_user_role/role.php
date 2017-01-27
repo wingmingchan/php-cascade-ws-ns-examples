982b786da9dd6524fa77c1525aa64cfe4d3bf981
Wing Ming Chan <chanw@upstate.edu>
2017-01-27 15:46:34 -0500

    Initial commit

---
+++ recipes/group_user_role/role.php
@@ -0,0 +1,37 @@
+<?php 
+require_once( 'auth_tutorial7.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $role = $cascade->getAsset( a\Role::TYPE, 10 );
+    
+    if( $role->getRoleType() == "global" )
+    {
+    	$abillities = $role->getGlobalAbilities();
+    }
+    else
+    {
+    	$abillities = $role->getSiteAbilities();
+    }
+    
+    u\DebugUtility::dump( $abillities );
+    $abillities->setAccessContentTypes( false );
+    $role->edit()->dump();
+    
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
