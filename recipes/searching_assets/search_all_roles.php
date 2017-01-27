afd122ed969f9e40e928d5f98f3c483199c3ba29
Wing Ming Chan <chanw@upstate.edu>
2017-01-27 14:54:45 -0500

    Initial commit

---
+++ recipes/searching_assets/search_all_roles.php
@@ -0,0 +1,27 @@
+<?php
+/*
+This program shows how to search for all roles.
+At most 250 roles will be returned.
+*/ 
+require_once( 'auth_tutorial7.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    u\DebugUtility::dump( $cascade->getRoles() );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
