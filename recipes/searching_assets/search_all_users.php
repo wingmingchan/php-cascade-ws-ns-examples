6920aa8fd2b2ed5eb876e64a5f7493124ca837d0
Wing Ming Chan <chanw@upstate.edu>
2017-01-27 10:42:34 -0500

    Initial commit

---
+++ recipes/searching_assets/search_all_users.php
@@ -0,0 +1,29 @@
+<?php
+/*
+This program shows how to search for all users.
+The number of users returned depends on how many groups there are in the system.
+*/
+require_once( 'auth_tutorial7.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $users = $cascade->getUsers();
+    
+    u\DebugUtility::dump( $users );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
