293eb72916584adda3dd53e863929563bfdc6774
Wing Ming Chan <chanw@upstate.edu>
2017-02-07 14:51:04 -0500

    Updated

--- recipes/phantom_nodes_values/report_phantom_nodes_type_b.php
+++ recipes/phantom_nodes_values/report_phantom_nodes_type_b.php
@@ -5,12 +5,7 @@ It can take a very long time to run.
 */
 $start_time = time();
 
-require_once( 'cascade_ws_ns/auth_chanw.php' );
-
-// to prevent time-out
-set_time_limit( 10000 );
-// to prevent using up memory when traversing a large site
-ini_set( 'memory_limit', '2048M' );
+require_once( 'auth_chanw.php' );
 
 use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
@@ -20,11 +15,13 @@ use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
 
 // site to be traverse
-$site_name = "cascade-admin"; // 636 seconds
+$site_name = "cascade-admin";
 $folder_id = "1e640d568b7f08ee4bf672732f23b0d5";
 
 try
 {
+    u\DebugUtility::setTimeSpaceLimits();
+
     $results = array();
     
     //$cascade->getSite( $site_name )->getBaseFolderAssetTree()->
@@ -38,21 +35,22 @@ try
     
     u\DebugUtility::dump( $results );
     
-    $end_time = time();
-    echo BR . "Total time taken: " . ( $end_time - $start_time ) . " seconds" . BR;
-
+    u\DebugUtility::outputDuration( $start_time );
 }
 catch( \Exception $e ) 
 {
     echo S_PRE . $e . E_PRE;
-    $end_time = time();
-    echo BR . "Total time taken: " . ( $end_time - $start_time ) . " seconds" . BR;
+    u\DebugUtility::outputDuration( $start_time );
 }
-
+catch( \Error $er ) 
+{
+    echo S_PRE . $er . E_PRE;
+    u\DebugUtility::outputDuration( $start_time );
+}
 
 function assetTreeReportPhantomNodes( 
     aohs\AssetOperationHandlerService $service, 
-    p\Child $child, $params=NULL, &$results=NULL )
+    p\Child $child, array $params=NULL, array &$results=NULL )
 {
     if( !isset( $results ) || !is_array( $results ) )
         throw new \Exception( "The results array is required" );
