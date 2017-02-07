e294d9cdf4a6782ca6d305509d72b1cae82d70fb
Wing Ming Chan <chanw@upstate.edu>
2017-02-07 14:55:39 -0500

    Updated

--- recipes/phantom_nodes_values/test_phantom_node_type_b.php
+++ recipes/phantom_nodes_values/test_phantom_node_type_b.php
@@ -1,5 +1,5 @@
 <?php 
-require_once('cascade_ws_ns/auth_chanw.php');
+require_once( 'auth_chanw.php' );
 
 use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
@@ -10,10 +10,16 @@ use cascade_ws_exception as e;
 
 try
 {
-    $cascade->getAsset( a\Page::TYPE, "1e64191a8b7f08ee4bf6727368416cbe" )->hasPhantomNodes();
+    echo u\StringUtility::boolToString(
+        $cascade->getAsset(
+            a\Page::TYPE, "1e64191a8b7f08ee4bf6727368416cbe" )->hasPhantomNodes() );
 }
 catch( \Exception $e ) 
 {
     echo S_PRE . $e . E_PRE; 
 } 
+catch( \Error $er ) 
+{
+    echo S_PRE . $er . E_PRE;
+}
 ?>
