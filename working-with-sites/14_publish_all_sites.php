1ef52b1e646a11cad5b6a8b77129c29a52d7b8f1
Wing Ming Chan <chanw@upstate.edu>
2017-02-16 09:43:13 -0500

    Initial commit

---
+++ working-with-sites/14_publish_all_sites.php
@@ -0,0 +1,31 @@
+<?php
+/*
+This program shows how to publish all sites.
+*/
+require_once( 'auth_chanw.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $sites = $cascade->getSites();
+    
+    foreach( $sites as $site )
+    {
+        $service->publish( $site->toStdClass() );
+    }
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
