1e9a71cae2fa4a357eff32ac7e4fe30e1afca5de
Wing Ming Chan <chanw@upstate.edu>
2017-08-09 15:20:57 -0400

    Initial commit

---
+++ tutorial/introductory/5/dd_get_identifiers.php
@@ -0,0 +1,24 @@
+<?php
+require_once( 'auth_tutorial7.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $dd = $cascade->getAsset( a\DataDefinition::TYPE, "ead5dd108b7ffe83164c9314c28a8074" );
+    u\DebugUtility::dump( $dd->getIdentifiers() );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
