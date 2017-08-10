9a0cd7036785085e053fb9d3840fbc5756a2ec4c
Wing Ming Chan <chanw@upstate.edu>
2017-08-10 08:33:04 -0400

    Initial commit

---
+++ tutorial/introductory/7/external_transformation.php
@@ -0,0 +1,30 @@
+<?php
+require_once( 'auth_chanw.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $xml  = $cascade->getAsset(
+	    a\XmlBlock::TYPE, "c1c150508b7f08ee4fe76bb8883e80b5" )->getXml();
+	$xsl  = $cascade->getAsset(
+	    a\XsltFormat::TYPE, "c1cdc7488b7f08ee4fe76bb8b0635d4a" )->getXml();
+    $xslt = new XSLTProcessor();
+    
+    $xslt->importStylesheet( new SimpleXMLElement( $xsl ) );
+    echo $xslt->transformToXml( new SimpleXMLElement( $xml ) );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
