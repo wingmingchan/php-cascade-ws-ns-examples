e1b764fe427f8aba2b169e23a00ebfabdad46179
Wing Ming Chan <chanw@upstate.edu>
2017-12-18 11:27:21 -0500

    Initial commit

---
+++ brisk/brisk_info.php
@@ -0,0 +1,10 @@
+<?php
+// Default metadata set
+$brisk_default_ms_id = "1cd91a998b7f08ee7df4e217a65462b1";
+// Page content type
+$brisk_page_ct_id    = "4224297e8b7f08ee0ed2ecda7b51db59";
+// Page configuration set
+$brisk_page_cs_id    = "421cac528b7f08ee0ed2ecda4e87e277";
+// Page data definition
+$brisk_page_dd_id    = "414f24e78b7f08ee0ed2ecda53e3e5bf";
+?>
