d32103c6bb3ddefd2b170165992323233181fc6a
Wing Ming Chan <chanw@upstate.edu>
2018-01-04 14:05:46 -0500

    Initial commit

---
+++ REST_SOAP/01role/03create_roles.php
@@ -0,0 +1,35 @@
+<?php
+require_once( 'auth_REST_SOAP.php' );
+require_once( 'role_constants.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $global_role = $cascade->createRole( $new_global_role_name, "global" );
+    $global_role->dump();
+    
+    $site_role = $cascade->createRole( $new_site_role_name, a\Site::TYPE );
+    $site_role->dump();
+    
+    if( !$soap )
+    {
+        u\DebugUtility::dump( $service->getCommands() );
+        // clean up
+        $service->clearCommands();
+    }
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
