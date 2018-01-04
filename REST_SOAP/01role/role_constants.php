e95ac583818f5a522cd7c3a4d6b2e8552797d44c
Wing Ming Chan <chanw@upstate.edu>
2018-01-04 14:03:20 -0500

    Initial commit

---
+++ REST_SOAP/01role/role_constants.php
@@ -0,0 +1,15 @@
+<?php
+// roles on sandbox
+if( $webapp )
+{
+	$site_role_id   = 50;
+	$global_role_id = 30;
+}
+// roles on production
+else
+{
+}
+
+$new_global_role_name = "Test WS Global Role";
+$new_site_role_name   = "Test WS Site Role";
+?>
