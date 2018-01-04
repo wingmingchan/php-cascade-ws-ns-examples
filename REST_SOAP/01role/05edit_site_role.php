925044f51afd5b0f6bb27fa76c1ab2f2d70eff63
Wing Ming Chan <chanw@upstate.edu>
2018-01-04 14:33:49 -0500

    Updated

--- REST_SOAP/01role/05edit_site_role.php
+++ REST_SOAP/01role/05edit_site_role.php
@@ -11,69 +11,69 @@ use cascade_ws_exception as e;
 
 try
 {
-	$site_role = $cascade->getAsset( a\Role::TYPE, 271 );
-	$site_abilities = $site_role->getSiteAbilities();
-	$site_abilities->
-	    setAccessAssetFactories( true )->
-	    setAccessAudits( true )->
-	    setAccessConfigurationSets( true )->
-	    setAccessConnectors( true )->
-	    setAccessContentTypes( true )->
-	    setAccessDataDefinitions( true )->
-	    setAccessDestinations( true )->
-	    setAccessEditorConfigurations( true )->
-	    setAccessManageSiteArea( true )->
-		setAccessMetadataSets( true )->
-		setAccessPublishSets( true )->
-		setAccessTransports( true )->
-		setAccessWorkflowDefinitions( true )->
-		setActivateDeleteVersions( true )->
-		setAlwaysAllowedToToggleDataChecks( true )->
-		setAssignApproveWorkflowSteps( true )->
-		setAssignWorkflowsToFolders( true )->
-		setBreakLocks( true )->
-		setBrokenLinkReportAccess( true )->
-		setBrokenLinkReportMarkFixed( true )->
-		setBulkChange( true )->
-		setBypassAllPermissionsChecks( true )->
-		setBypassAssetFactoryGroupsNewMenu( true )->
-		setBypassDestinationGroupsWhenPublishing( true )->
-		setBypassWorkflow( true )->
-		setBypassWorkflowDefintionGroupsForFolders( true )->
-		setBypassWysiwygEditorRestrictions( true )->
-		setCancelPublishJobs( true )->
-		setDeleteWorkflows( true )->
-		setDiagnosticTests( true )->
-		setEditAccessRights( true )->
-		setEditDataDefinition( true )->
-		setEditPageContentType( true )->
-		setEditPageLevelConfigurations( true )->
-		setImportZipArchive( true )->
-		setMoveRenameAssets( true )->
-		setMultiSelectCopy( true )->
-		setMultiSelectDelete( true )->
-		setMultiSelectMove( true )->
-		setMultiSelectPublish( true )->
-		setPublishReadableAdminAreaAssets( true )->
-		setPublishReadableHomeAssets( true )->
-		setPublishWritableAdminAreaAssets( true )->
-		setPublishWritableHomeAssets( true )->
-		setRecycleBinDeleteAssets( true )->
-		setRecycleBinViewRestoreAllAssets( true )->
-		setRecycleBinViewRestoreUserAssets( true )->
-		setReorderPublishQueue( true )->
-		setSendStaleAssetNotifications( true )->
-		setUploadImagesFromWysiwyg( true )->
-		setViewPublishQueue( true )->
-		setViewVersions( true );
-	$site_role->edit()->dump();
+    $site_role = $cascade->getAsset( a\Role::TYPE, 271 );
+    $site_abilities = $site_role->getSiteAbilities();
+    $site_abilities->
+        setAccessAssetFactories( true )->
+        setAccessAudits( true )->
+        setAccessConfigurationSets( true )->
+        setAccessConnectors( true )->
+        setAccessContentTypes( true )->
+        setAccessDataDefinitions( true )->
+        setAccessDestinations( true )->
+        setAccessEditorConfigurations( true )->
+        setAccessManageSiteArea( true )->
+        setAccessMetadataSets( true )->
+        setAccessPublishSets( true )->
+        setAccessTransports( true )->
+        setAccessWorkflowDefinitions( true )->
+        setActivateDeleteVersions( true )->
+        setAlwaysAllowedToToggleDataChecks( true )->
+        setAssignApproveWorkflowSteps( true )->
+        setAssignWorkflowsToFolders( true )->
+        setBreakLocks( true )->
+        setBrokenLinkReportAccess( true )->
+        setBrokenLinkReportMarkFixed( true )->
+        setBulkChange( true )->
+        setBypassAllPermissionsChecks( true )->
+        setBypassAssetFactoryGroupsNewMenu( true )->
+        setBypassDestinationGroupsWhenPublishing( true )->
+        setBypassWorkflow( true )->
+        setBypassWorkflowDefintionGroupsForFolders( true )->
+        setBypassWysiwygEditorRestrictions( true )->
+        setCancelPublishJobs( true )->
+        setDeleteWorkflows( true )->
+        setDiagnosticTests( true )->
+        setEditAccessRights( true )->
+        setEditDataDefinition( true )->
+        setEditPageContentType( true )->
+        setEditPageLevelConfigurations( true )->
+        setImportZipArchive( true )->
+        setMoveRenameAssets( true )->
+        setMultiSelectCopy( true )->
+        setMultiSelectDelete( true )->
+        setMultiSelectMove( true )->
+        setMultiSelectPublish( true )->
+        setPublishReadableAdminAreaAssets( true )->
+        setPublishReadableHomeAssets( true )->
+        setPublishWritableAdminAreaAssets( true )->
+        setPublishWritableHomeAssets( true )->
+        setRecycleBinDeleteAssets( true )->
+        setRecycleBinViewRestoreAllAssets( true )->
+        setRecycleBinViewRestoreUserAssets( true )->
+        setReorderPublishQueue( true )->
+        setSendStaleAssetNotifications( true )->
+        setUploadImagesFromWysiwyg( true )->
+        setViewPublishQueue( true )->
+        setViewVersions( true );
+    $site_role->edit()->dump();
 
-	if( !$soap )
-	{
-		u\DebugUtility::dump( $service->getCommands() );
-		// clean up
-		$service->clearCommands();
-	}
+    if( !$soap )
+    {
+        u\DebugUtility::dump( $service->getCommands() );
+        // clean up
+        $service->clearCommands();
+    }
 }
 catch( \Exception $e ) 
 {
@@ -81,6 +81,6 @@ catch( \Exception $e )
 }
 catch( \Error $er )
 {
-	echo S_PRE . $er . E_PRE; 
+    echo S_PRE . $er . E_PRE; 
 }
 ?>
