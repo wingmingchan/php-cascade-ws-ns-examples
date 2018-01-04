450be6c4e721b445d7ffc5b3cff2235d6c5bb0f6
Wing Ming Chan <chanw@upstate.edu>
2018-01-04 14:05:15 -0500

    Initial commit

---
+++ REST_SOAP/01role/01get_role_type.php
@@ -0,0 +1,42 @@
+<?php
+/*
+A role can be either global or site. They define different abilities.
+Use getRoleType to get the type string.
+*/
+require_once( 'auth_REST_SOAP.php' );
+require_once( 'role_constants.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    // global
+    //$role = $cascade->getRole( $global_role_id );
+    $role = $cascade->getRole( 5 );
+    echo $role->getRoleType(), BR;
+    
+    // site
+    //$role = $cascade->getRole( $site_role_id );
+    //echo $role->getRoleType(), BR;
+    
+    if( !$soap )
+    {
+        u\DebugUtility::dump( $service->getCommands() );
+        // clean up
+        $service->clearCommands();
+    }
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
