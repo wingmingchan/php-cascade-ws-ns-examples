6a98f61cd6b0d853aa2a17552578e25eee8b8546
Wing Ming Chan <chanw@upstate.edu>
2018-01-05 12:18:40 -0500

    Initial commit

---
+++ REST_SOAP/03user/01create_user.php
@@ -0,0 +1,34 @@
+<?php
+require_once( 'auth_REST_SOAP.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $user_name = "test-ws-user";
+    $user      = $cascade->getUser( $user_name );
+    $group     = $cascade->getAsset( a\Group::TYPE, "test-ws-group" );
+    $role      = $cascade->getAsset( a\Role::TYPE, 10 );
+
+    if( isset( $user ) )
+        $cascade->deleteAsset( $user );
+    
+    $user = $cascade->createUser( $user_name, "password", $group, $role );
+    $user->dump();
+    
+    u\DebugUtility::dumpRESTCommands( $service );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
