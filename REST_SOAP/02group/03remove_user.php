55f467ec85a1d00bfc37e686b4590ba245ada6eb
Wing Ming Chan <chanw@upstate.edu>
2018-01-05 10:17:30 -0500

    Initial commit

---
+++ REST_SOAP/02group/03remove_user.php
@@ -0,0 +1,26 @@
+<?php
+require_once( 'auth_REST_SOAP.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $group = $cascade->getAsset( a\Group::TYPE, "test-ws-group" );
+    $group->removeUserName( "wing" )->edit()->dump();
+    
+    u\DebugUtility::dumpRESTCommands( $service );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
