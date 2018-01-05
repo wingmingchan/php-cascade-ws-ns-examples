7f2618a89d0f589be4208b98f90714691980366e
Wing Ming Chan <chanw@upstate.edu>
2018-01-05 12:18:57 -0500

    Initial commit

---
+++ REST_SOAP/03user/02change_password.php
@@ -0,0 +1,27 @@
+<?php
+require_once( 'auth_REST_SOAP.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $user = $cascade->getAsset( a\User::TYPE, "test-ws-user" );
+    $user->setPassword( "NEWPASSWORD" )->edit();
+    $user->dump();
+    
+    u\DebugUtility::dumpRESTCommands( $service );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
