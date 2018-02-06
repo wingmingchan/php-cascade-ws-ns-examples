c2cabfd9adc7ae6606980268dfeb90ca34fbc86c
Wing Ming Chan <chanw@upstate.edu>
2018-02-06 12:52:45 -0500

    Initial commit

---
+++ admin/remove_phantom_nodes.php
@@ -0,0 +1,32 @@
+<?php
+require_once( 'auth_REST_SOAP.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+$site_name = "formats";
+
+$start_time = time();
+
+try
+{
+    u\DebugUtility::setTimeSpaceLimits();
+    
+    $results = array();
+    $admin->removePhantomNodes( $site_name, "velocity", $results );
+    u\DebugUtility::dump( $results );
+    u\DebugUtility::outputDuration( $start_time );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
