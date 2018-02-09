11892c4f00a7b3184083a28b8570ecca9a27ca63
Wing Ming Chan <chanw@upstate.edu>
2018-02-09 14:33:00 -0500

    Initial commit

---
+++ admin/clear_access.php
@@ -0,0 +1,33 @@
+<?php
+require_once( 'auth_REST_SOAP.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+$site_name   = "formats";
+$folder_path = "xslt/building-library/java-extension";
+$applied_to_children = true;
+
+$start_time = time();
+
+try
+{
+    u\DebugUtility::setTimeSpaceLimits();
+    
+    $admin->clearFolderAccess( $site_name, $folder_path, $applied_to_children );
+
+    u\DebugUtility::outputDuration( $start_time );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
