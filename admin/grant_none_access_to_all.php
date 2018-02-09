429ef5ddc19e81372450d620db40fcd6f645e308
Wing Ming Chan <chanw@upstate.edu>
2018-02-09 15:08:13 -0500

    Initial commit

---
+++ admin/grant_none_access_to_all.php
@@ -0,0 +1,32 @@
+<?php
+require_once( 'auth_REST_SOAP.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+$site_name   = "formats";
+$applied_to_children = true;
+
+$start_time = time();
+
+try
+{
+    u\DebugUtility::setTimeSpaceLimits();
+    
+    $admin->grantSiteNoneAccessToAll( $site_name, $applied_to_children );
+
+    u\DebugUtility::outputDuration( $start_time );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
