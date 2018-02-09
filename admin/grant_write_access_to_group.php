09bc8c2f9a45445ee3e61e9e91fa6eba9ee204ee
Wing Ming Chan <chanw@upstate.edu>
2018-02-09 14:56:38 -0500

    Initial commit

---
+++ admin/grant_write_access_to_group.php
@@ -0,0 +1,35 @@
+<?php
+require_once( 'auth_REST_SOAP.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+$site_name   = "formats";
+$folder_path = "xslt/building-library/java-extension";
+$applied_to_children = true;
+
+$start_time = time();
+
+try
+{
+    u\DebugUtility::setTimeSpaceLimits();
+    
+    $admin->grantFolderWriteAccessToUserGroup(
+        $site_name, $folder_path, $applied_to_children,
+        $cascade->getAsset( a\Group::TYPE, "CWT-Designers" ) );
+
+    u\DebugUtility::outputDuration( $start_time );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
