2c9ba1aed1fade8394c0048a8d492289e2d810dd
Wing Ming Chan <chanw@upstate.edu>
2018-08-21 15:14:45 -0400

    Initial commit

---
+++ tutorial/introductory/2/show_json_commands.php
@@ -0,0 +1,24 @@
+<?php
+require_once( 'auth_REST_SOAP.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $block = $cascade->getAsset( a\TextBlock::TYPE, "93ba9ccc8b7f08ee16e20663e378a1fe" );
+    u\DebugUtility::dump( $service->getCommands() );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
