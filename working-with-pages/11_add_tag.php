6b0120ec3dcf8fc1e6d642d74a44c54d33f33895
Wing Ming Chan <chanw@bigimac.upstate.edu>
2019-01-10 14:17:06 -0500

    Initial commit

---
+++ working-with-pages/11_add_tag.php
@@ -0,0 +1,29 @@
+<?php
+/*
+This program adds a tag to a page.
+*/
+
+require_once( 'auth_soap_c8.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+try
+{
+    $page = $admin->getAsset( a\Page::TYPE, "681da2ca7f00000161e16fb03983fb1d" );
+    $page->addTag( "News" )->edit();
+    $page->dump();
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
