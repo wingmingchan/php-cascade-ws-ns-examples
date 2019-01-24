cc7aee81d9ac71d261844df3e2b51c5d596206fc
wingmingchan <chanw@upstate.edu>
2019-01-24 15:24:25 -0500

    Initial commit

---
+++ update/update_text_block.php
@@ -0,0 +1,33 @@
+<?php
+require_once( 'auth_REST_SOAP.php' );
+
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_constants as c;
+use cascade_ws_asset     as a;
+use cascade_ws_property  as p;
+use cascade_ws_utility   as u;
+use cascade_ws_exception as e;
+
+$start_time = time();
+
+try
+{
+    $block = $admin->getAsset( a\TextBlock::TYPE, "0ac3696a8b7f08ee5a4fada2072faed1" )->update(
+        array(
+            // text block data
+            a\DublinAwareAsset::TEXT => "Hello, World!"
+        )
+    )->dump();
+
+    $end_time = time();
+    echo "\nTotal time taken: " . ( $end_time - $start_time ) . " seconds\n";
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE; 
+}
+?>
