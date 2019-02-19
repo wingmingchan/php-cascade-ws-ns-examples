41039e970bb51dde39b9e8ea90601402187c98a7
wingmingchan <chanw@upstate.edu>
2019-02-19 09:44:45 -0500

    Initial commit

---
+++ tutorials/advanced/01raw/edit_page_h1_rest.php
@@ -0,0 +1,37 @@
+<?php
+$u = "admin";
+$p = "admin";
+
+// url
+$url = "http://mydomain.edu:1234/api/v1/read/page/formats/index?u=$u&p=$p";
+
+$reply = json_decode( file_get_contents( $url ) );
+
+if( $reply->success )
+{
+    $reply->asset->page->structuredData->structuredDataNodes[ 1 ]->
+        structuredDataNodes[ 2 ]->text = "Formats";
+    $url = "http://mydomain.edu:1234/api/v1/edit?u=$u&p=$p";
+    $reply = apiOperation( $url, array( 'asset' => $reply->asset ) );
+    var_dump( $reply );
+}
+
+function apiOperation( $url, $params )
+{
+    return json_decode(
+        file_get_contents(
+            $url, 
+            false, 
+            stream_context_create(
+                array( 
+                    'http' => array(
+                        'header'  => "Content-Type: application/x-www-form-urlencoded\r\n",
+                        'method'  => 'POST',
+                        'content' => json_encode( $params )
+                    )
+                )
+            )
+        )
+    );
+}
+?>
