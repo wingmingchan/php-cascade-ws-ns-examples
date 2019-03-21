eafc48566b18d43927648e303704409de7c056af
wingmingchan <chanw@upstate.edu>
2019-03-21 09:23:12 -0400

    Updated

--- tutorials/introductory/2/show_rest_commands.php
+++ tutorials/introductory/2/show_rest_commands.php
@@ -1,5 +1,5 @@
 <?php
-require_once( 'auth_REST_SOAP.php' );
+require_once( 'auth_rest_c8.php' );
 
 use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
@@ -10,7 +10,7 @@ use cascade_ws_exception as e;
 
 try
 {
-    $block = $cascade->getAsset( a\TextBlock::TYPE, "93ba9ccc8b7f08ee16e20663e378a1fe" );
+    $block = $admin->getAsset( a\TextBlock::TYPE, "7cd5a023ac1e001b5dd3d5e8ed91990c" );
     u\DebugUtility::dump( $service->getCommands() );
 }
 catch( \Exception $e ) 
