913904128e9f81ddf95441a52495acf91a57be04
wingmingchan <chanw@upstate.edu>
2019-03-21 15:16:20 -0400

    Updated

--- tutorials/introductory/3/manipulate_dynamic_metadata_field_definition.php
+++ tutorials/introductory/3/manipulate_dynamic_metadata_field_definition.php
@@ -1,5 +1,5 @@
 <?php
-require_once( 'auth_tutorial7.php' );
+require_once( 'auth_REST_SOAP.php' );
 
 use cascade_ws_AOHS      as aohs;
 use cascade_ws_constants as c;
@@ -10,13 +10,13 @@ use cascade_ws_exception as e;
 
 try
 {
-    $ms = $cascade->getAsset( a\MetadataSet::TYPE, 'b893fd058b7f0856002a5e11185ff809' );
+    $ms = $cascade->getAsset( a\MetadataSet::TYPE, 'a15e54ae8b7ffe837b9f2648f7d70d16' );
     
     // dump the string array of dynamic field names
     u\DebugUtility::dump( $ms->getDynamicMetadataFieldDefinitionNames() );
 
     // find out if a field exists
-    $field_name = 'exclude-from-left';
+    $field_name = 'exclude-from-left-folder-nav';
     u\DebugUtility::out( "The field $field_name " .
         ( $ms->hasDynamicMetadataFieldDefinition( $field_name ) ? 
             "exists" : "does not exist"  ) );
@@ -30,35 +30,35 @@ try
         // Type: checkbox
         u\DebugUtility::out( 
             BR . "Type: " . $dmfd->getFieldType() . BR . "Possible values: " );
-        // Possible values: array( Yes, No )
+        // Possible values: array( yes, no )
         u\DebugUtility::dump( $dmfd->getPossibleValueStrings() );
         
         // add another possible value at the end
-        u\DebugUtility::out( "Appending Maybe" );
-        $dmfd->appendValue( "Maybe" );
+        u\DebugUtility::out( "Appending maybe" );
+        $dmfd->appendValue( "maybe" );
         $ms->edit(); // commit the change!!!
         u\DebugUtility::dump( $dmfd->getPossibleValueStrings() );
         
-        // swap No and Maybe
-        u\DebugUtility::out( "Swapping No and Maybe" );
-        $dmfd->swapValues( "No", "Maybe" );
+        // swap no and maybe
+        u\DebugUtility::out( "Swapping no and maybe" );
+        $dmfd->swapValues( "no", "maybe" );
         $ms->edit(); // commit the change!!!
         u\DebugUtility::dump( $dmfd->getPossibleValueStrings() );
         
-        // maybe not, delete Maybe
-        u\DebugUtility::out( "Removing Maybe" );
-        $dmfd->removeValue( "Maybe" );
+        // maybe not, delete maybe
+        u\DebugUtility::out( "Removing maybe" );
+        $dmfd->removeValue( "maybe" );
         $ms->edit(); // commit the change!!!
         u\DebugUtility::dump( $dmfd->getPossibleValueStrings() );
     
         // set selected by default for No
-        $dmfd->setSelectedByDefault( "No" );
+        $dmfd->setSelectedByDefault( "no" );
         $ms->edit();  // commit the change!!!
         
-        // check if Maybe still exists
+        // check if maybe still exists
         u\DebugUtility::out(
-            "The possible value 'Maybe' " .
-            ( $dmfd->hasPossibleValue( "Maybe" ) ? "exists" : "does not exist" )
+            "The possible value 'maybe' " .
+            ( $dmfd->hasPossibleValue( "maybe" ) ? "exists" : "does not exist" )
         );
     }
 }
