04307f72702a132e05386c9f204543de05d5c681
wingmingchan <chanw@upstate.edu>
2019-05-07 08:43:38 -0400

    Initial commit

--- ws-api-documentation/wsdl_viewer.php
+++ ws-api-documentation/wsdl_viewer.php
@@ -1,6 +1,6 @@
 <?php
-$xml_url   = "http://mydomain.edu:1234/ws/services/AssetOperationService?wsdl";
-$xsl_url   = "https://raw.githubusercontent.com/qvantel/wsdl-viewer/master/wsdl-viewer.xsl";
+$xml_url = "http://mydomain.edu:1234/ws/services/AssetOperationService?wsdl";
+$xsl_url = "https://raw.githubusercontent.com/qvantel/wsdl-viewer/master/wsdl-viewer.xsl";
 
 try
 {
